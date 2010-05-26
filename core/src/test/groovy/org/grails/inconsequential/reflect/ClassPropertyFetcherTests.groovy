d1297c056fe0ff6b78e5402da47f51d9eaf18b6b
Graeme Rocher <graeme.rocher@gmail.com>
2010-05-26 14:48:28 +0200

    Added support for Groovy and wrote first test

--- core/src/test/groovy/org/grails/inconsequential/reflect/ClassPropertyFetcherTests.groovy
+++ core/src/test/groovy/org/grails/inconsequential/reflect/ClassPropertyFetcherTests.groovy
@@ -1,8 +1,21 @@
 package org.grails.inconsequential.reflect
 
+import org.junit.Test
+
 /**
  * @author Graeme Rocher
  * @since 1.1
  */
-class ClassPropertyFetcherTests {
+class ClassPropertyFetcherTests  {
+
+  @Test
+  void testGetProperty() {
+      def cpf = ClassPropertyFetcher.forClass(Foo)
+
+      assert 'foo' == cpf.getPropertyValue("name")
+  }
+
+  static class Foo {
+      static String name = "foo"
+  }
 }
