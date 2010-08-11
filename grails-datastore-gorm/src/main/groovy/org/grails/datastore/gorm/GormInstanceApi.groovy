972b2ee5a0ff66bf1a4e54732d883a89ed2a0352
Graeme Rocher <graeme.rocher@gmail.com>
2010-08-11 17:01:49 +0200

    Started to flesh out GORM-style API tested against Redis. CRUD operations working.

---
+++ grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/GormInstanceApi.groovy
@@ -0,0 +1,40 @@
+/* Copyright (C) 2010 SpringSource
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.datastore.gorm
+
+import org.springframework.datastore.core.Datastore
+
+/**
+ * Instance methods of the GORM API
+ *
+ * @author Graeme Rocher
+ */
+class GormInstanceApi {
+  Datastore datastore
+
+  GormInstanceApi(Datastore datastore) {
+    this.datastore = datastore;
+  }
+
+  def save(instance) {
+    datastore.currentSession.persist(instance)
+    return instance
+  }
+
+  def delete(instance) {
+    datastore.currentSession.delete(instance)
+  }
+
+}
