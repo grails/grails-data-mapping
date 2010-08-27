e9970fe96b99dcee693088c8494b1de2defe98fe
Graeme Rocher <graeme.rocher@gmail.com>
2010-08-27 18:59:30 +0200

    fixes for some compilation errors referencing no longer needed dependency

--- spring-datastore-redis/src/main/java/org/springframework/datastore/redis/util/RedisTemplate.java
+++ spring-datastore-redis/src/main/java/org/springframework/datastore/redis/util/RedisTemplate.java
@@ -14,7 +14,6 @@
  */
 package org.springframework.datastore.redis.util;
 
-import sma.RedisClient;
 
 import java.util.List;
 import java.util.Map;
--- spring-datastore-redis/src/main/java/org/springframework/datastore/redis/util/SortParams.java
+++ spring-datastore-redis/src/main/java/org/springframework/datastore/redis/util/SortParams.java
@@ -1,16 +1,28 @@
+/* Copyright (C) 2010 SpringSource
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.datastore.redis.util;
 
-import sma.RedisClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by IntelliJ IDEA.
- * User: graemerocher
- * Date: Aug 24, 2010
- * Time: 4:04:59 PM
- * To change this template use File | Settings | File Templates.
+ * Parameters used for sorting.
+ * 
+ * @author Graeme Rocher
  */
 public abstract class SortParams<T> {
 
