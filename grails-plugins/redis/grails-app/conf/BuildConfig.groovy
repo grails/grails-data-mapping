438b2d8582e72754d6702f6dd5aec6798751bdb2
Graeme Rocher <graeme.rocher@gmail.com>
2010-10-20 10:09:21 -0500

    Updated version for M2 release

--- grails-plugins/redis/grails-app/conf/BuildConfig.groovy
+++ grails-plugins/redis/grails-app/conf/BuildConfig.groovy
@@ -25,9 +25,9 @@ grails.project.dependency.resolution = {
         excludes "slf4j-simple", "persistence-api", "commons-logging", "jcl-over-slf4j", "slf4j-api", "jta"
         excludes "spring-core", "spring-beans", "spring-aop", "spring-tx", "spring-context", "spring-web"
       }
-      compile("org.grails:grails-datastore-gorm-redis:1.0.0.BUILD-SNAPSHOT", excludes)
-      compile( "org.springframework:spring-datastore-web:1.0.0.BUILD-SNAPSHOT", excludes)
-      test("org.grails:grails-datastore-gorm-test:1.0.0.BUILD-SNAPSHOT", excludes)
+      compile("org.grails:grails-datastore-gorm-redis:1.0.0.M2", excludes)
+      compile( "org.springframework:spring-datastore-web:1.0.0.M2", excludes)
+      test("org.grails:grails-datastore-gorm-test:1.0.0.M2", excludes)
 
     }
 }
