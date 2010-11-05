64624535f4665c13ab0818fa7c4cee9957deb275
J. Brisbin <jon@jbrisbin.com>
2010-11-05 15:10:27 -0500

    Added test suite class for controlling Riak TCK tests

---
+++ grails-datastore-gorm-riak/src/test/groovy/org/grails/datastore/gorm/riak/RiakSuite.groovy
@@ -0,0 +1,40 @@
+package org.grails.datastore.gorm.riak
+
+import grails.gorm.tests.*
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.junit.runners.Suite.SuiteClasses
+
+/**
+ * @author Jon Brisbin <jon.brisbin@npcinternational.com>
+ */
+@RunWith(Suite)
+@SuiteClasses([
+//  RangeQuerySpec,
+//  NamedQuerySpec,
+CriteriaBuilderSpec,
+//  OrderBySpec,
+//  CommonTypesPersistenceSpec,
+//  QueryAfterPropertyChangeSpec,
+//  QueryByAssociationSpec,
+//  UpdateWithProxyPresentSpec,
+//  DomainEventsSpec,
+//  ProxyLoadingSpec,
+//  QueryAfterPropertyChangeSpec,
+//  CircularOneToManySpec,
+//  InheritanceSpec,
+//  FindByMethodSpec,
+//  ListOrderBySpec,
+//  GroovyProxySpec,
+//  CommonTypesPersistenceSpec,
+//  GormEnhancerSpec,
+//  NegationSpec,
+//
+//  ValidationSpec,
+//  UpdateWithProxyPresentSpec,
+//  AttachMethodSpec,
+//  WithTransactionSpec,
+CrudOperationsSpec
+])
+class RiakSuite {
+}
