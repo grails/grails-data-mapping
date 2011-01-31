8d179dd9d833d42d9816b3447144942c31812018
rvanderwerf <rvanderwerf@gmail.com>
2011-01-30 21:55:11 -0600

    Fixed several failing tests

---
+++ grails-datastore-gorm-jcr/src/test/groovy/grails/gorm/tests/CommonTypes.groovy
@@ -0,0 +1,19 @@
+@grails.persistence.Entity
+class CommonTypes implements Serializable{
+  Long id
+  Long l
+  Byte b
+  Short s
+  Boolean bool
+  Integer i
+  URL url
+  Date date
+  Calendar c
+  BigDecimal bd
+  BigInteger bi
+  Double d
+  Float f
+  TimeZone tz
+  Locale loc
+  Currency cur
+}
