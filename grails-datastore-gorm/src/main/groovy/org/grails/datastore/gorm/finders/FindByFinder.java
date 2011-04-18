51da589c5e115f60dac46b19f816bbc959fbb051
Jeff Brown <jeff@jeffandbetsy.net>
2011-04-18 14:27:24 -0500

    code related to findOrCreateBy*

--- grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/finders/FindByFinder.java
+++ grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/finders/FindByFinder.java
@@ -14,78 +14,18 @@
  */
 package org.grails.datastore.gorm.finders;
 
-import java.util.List;
 import java.util.regex.Pattern;
 
 import org.springframework.datastore.mapping.core.Datastore;
-import org.springframework.datastore.mapping.core.Session;
-import org.springframework.datastore.mapping.core.SessionCallback;
-import org.springframework.datastore.mapping.query.Query;
 
 /**
  * Finder used to return a single result
  */
-public class FindByFinder extends DynamicFinder {
+public class FindByFinder extends AbstractFindByFinder {
 
-    private static final String OPERATOR_OR = "Or";
-    private static final String OPERATOR_AND = "And";
     private static final String METHOD_PATTERN = "(findBy)([A-Z]\\w*)";
-    private static final String[] OPERATORS = new String[]{ OPERATOR_AND, OPERATOR_OR };
 
     public FindByFinder(final Datastore datastore) {
-        super(Pattern.compile(METHOD_PATTERN), OPERATORS, datastore);
-    }
-
-    @Override
-    protected Object doInvokeInternal(final DynamicFinderInvocation invocation) {
-        return execute(new SessionCallback<Object>() {
-            public Object doInSession(final Session session) {
-                Query q = buildQuery(invocation, session);
-                return invokeQuery(q);
-            }
-        });
-    }
-
-    protected Object invokeQuery(Query q) {
-        q.max(1);
-
-        List results = q.list();
-        if (results.isEmpty()) {
-            return null;
-        }
-
-        return results.get(0);
-    }
-
-    private boolean firstExpressionIsRequiredBoolean() {
-        return false;
-    }
-
-    public Query buildQuery(DynamicFinderInvocation invocation, Session session) {
-        final Class clazz = invocation.getJavaClass();
-        Query q = session.createQuery(clazz);
-        applyAdditionalCriteria(q, invocation.getCriteria());
-        configureQueryWithArguments(clazz, q, invocation.getArguments());
-
-        final String operatorInUse = invocation.getOperator();
-
-        if (operatorInUse != null && operatorInUse.equals(OPERATOR_OR)) {
-            if (firstExpressionIsRequiredBoolean()) {
-                MethodExpression expression = invocation.getExpressions().remove(0);
-                q.add(expression.createCriterion());
-            }
-
-            Query.Junction disjunction = q.disjunction();
-
-            for (MethodExpression expression : invocation.getExpressions()) {
-                disjunction.add(expression.createCriterion());
-            }
-        }
-        else {
-            for (MethodExpression expression : invocation.getExpressions()) {
-                q.add( expression.createCriterion() );
-            }
-        }
-        return q;
+        super(Pattern.compile(METHOD_PATTERN), datastore);
     }
 }
