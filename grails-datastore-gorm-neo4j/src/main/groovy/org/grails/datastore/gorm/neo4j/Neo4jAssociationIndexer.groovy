24a3749dd075f76b9c786f749bc2e4b7b1cc6093
Stefan Armbruster <stefan@armbruster-it.de>
2011-05-13 15:40:56 +0200

    Bugfix: removed creation of duplicate relationships

--- grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/Neo4jAssociationIndexer.groovy
+++ grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/Neo4jAssociationIndexer.groovy
@@ -1,16 +1,14 @@
 package org.grails.datastore.gorm.neo4j
 
+import org.neo4j.graphdb.Direction
+import org.neo4j.graphdb.DynamicRelationshipType
+import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.graphdb.Node
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.datastore.mapping.engine.AssociationIndexer
 import org.springframework.datastore.mapping.model.PersistentEntity
 import org.springframework.datastore.mapping.model.types.Association
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.apache.commons.lang.NotImplementedException
-import org.neo4j.graphdb.Node
-import org.neo4j.graphdb.DynamicRelationshipType
-import org.neo4j.graphdb.GraphDatabaseService
-import org.neo4j.graphdb.Direction
-import org.neo4j.graphdb.Relationship
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,7 +19,7 @@ import org.neo4j.graphdb.Relationship
  */
 class Neo4jAssociationIndexer implements AssociationIndexer {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Neo4jAssociationIndexer.class);
+    private static final Logger log = LoggerFactory.getLogger(Neo4jAssociationIndexer.class);
 
     Node nativeEntry
     Association association
@@ -36,9 +34,11 @@ class Neo4jAssociationIndexer implements AssociationIndexer {
     }
 
     List query(Object primaryKey) {
-        def ids = nativeEntry.getRelationships(relationshipType)
-            .collect { it.getOtherNode(nativeEntry).id}
-	    LOG.info("query $primaryKey: $ids")
+        def ids = nativeEntry.getRelationships(relationshipType).collect {
+            log.debug "relation: $it.startNode -> $it.endNode $it.type"
+            it.getOtherNode(nativeEntry).id
+        }
+	    log.info("query $primaryKey: $ids")
 	    ids
     }
 
@@ -48,15 +48,29 @@ class Neo4jAssociationIndexer implements AssociationIndexer {
 
     void index(Object primaryKey, Object foreignKey) {
         if (primaryKey!=foreignKey) {
-            def keyOfOtherNode = (primaryKey == nativeEntry.id) ? foreignKey : primaryKey
+
+            def startNode = graphDatabaseService.getNodeById(primaryKey)
+            def endNode = graphDatabaseService.getNodeById(foreignKey)
+
+            def hasRelationship = startNode.getRelationships(relationshipType, Direction.OUTGOING).any { it.endNode == endNode }
+            if (!hasRelationship) {
+                def rel = startNode.createRelationshipTo(endNode, relationshipType)
+                log.info("createRelationship $rel.startNode.id -> $rel.endNode.id ($rel.type)")
+
+            }
+
+            /*def keyOfOtherNode = (primaryKey == nativeEntry.id) ? foreignKey : primaryKey
             def target = graphDatabaseService.getNodeById(keyOfOtherNode)
+
 
             def hasRelationship = nativeEntry.getRelationships(relationshipType, Direction.OUTGOING).any { it.endNode == target}
             if (!hasRelationship) {
                 def rel = nativeEntry.createRelationshipTo(target, relationshipType)
-                LOG.warn("createRelationship $rel.startNode.id -> $rel.endNode.id ($rel.type)")
+                log.warn("createRelationship $rel.startNode.id -> $rel.endNode.id ($rel.type)")
 
-            }
+            } */
+        } else {
+            log.warn "selfreferecing is not yet supported"
         }
     }
 
