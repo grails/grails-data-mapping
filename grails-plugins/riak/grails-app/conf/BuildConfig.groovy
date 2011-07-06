d740d3bb7d606e496e5706854197601edc0accba
Thomas Risberg <trisberg@vmware.com>
2011-07-06 00:02:52 -0400

    undid some exclude changes for slf4j-log4j12

--- grails-plugins/riak/grails-app/conf/BuildConfig.groovy
+++ grails-plugins/riak/grails-app/conf/BuildConfig.groovy
@@ -24,7 +24,7 @@ grails.project.dependency.resolution = {
 
         def excludes = {
             excludes "slf4j-simple", "persistence-api", "commons-logging", "jcl-over-slf4j", "slf4j-api", "jta"
-            excludes "spring-core", "spring-beans", "spring-aop", "spring-asm", "spring-webmvc", "spring-tx", "spring-context", "spring-web", "log4j", "slf4j-simple"
+            excludes "spring-core", "spring-beans", "spring-aop", "spring-asm", "spring-webmvc", "spring-tx", "spring-context", "spring-web", "log4j", "slf4j-log4j12"
         }
         runtime("org.springframework.data:spring-data-riak:1.0.0.M2", excludes)
         runtime("org.grails:grails-datastore-gorm:1.0.0.M4", excludes)
