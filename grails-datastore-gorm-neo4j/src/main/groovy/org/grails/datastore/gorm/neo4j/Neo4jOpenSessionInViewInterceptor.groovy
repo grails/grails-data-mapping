577b5492d2f2439fa36df571955ba38232a9f75a
Stefan Armbruster <stefan@armbruster-it.de>
2011-07-13 12:16:30 +0200

    neo4j: support for nested invocations (this happens when using <g:include>) of Neo4jOpenSessionInViewInterceptor, transactions are stored in a stack

--- grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/Neo4jOpenSessionInViewInterceptor.groovy
+++ grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/Neo4jOpenSessionInViewInterceptor.groovy
@@ -1,38 +1,36 @@
 package org.grails.datastore.gorm.neo4j
 
 import org.springframework.web.context.request.WebRequest
-import org.springframework.ui.ModelMap
 import org.springframework.datastore.mapping.web.support.OpenSessionInViewInterceptor
-import org.springframework.datastore.mapping.core.DatastoreUtils;
+import org.springframework.datastore.mapping.core.DatastoreUtils
+import org.slf4j.LoggerFactory
+import org.slf4j.Logger
+import org.springframework.datastore.mapping.transactions.Transaction
 
 /**
- * Created by IntelliJ IDEA.
- * User: stefan
- * Date: 08.05.11
- * Time: 23:15
- * To change this template use File | Settings | File Templates.
+ * provide a transaction context for each request
+ * since the interceptor might be called multiple times for the same request (by e.g. using <g:include>)
+ * all previous transactions must be kept for releasing them later. For this a Stack is used.
  */
 class Neo4jOpenSessionInViewInterceptor extends OpenSessionInViewInterceptor {
 
-    def transaction
+    private static final Logger log = LoggerFactory.getLogger(Neo4jOpenSessionInViewInterceptor.class)
+    Stack<Transaction> transactions = new Stack()
 
     @Override
     void preHandle(WebRequest request) {
+        log.debug "preHandle ${request.getDescription(true)}"
         super.preHandle(request)
         def session = DatastoreUtils.getSession(datastore, true)
-        transaction = session.beginTransaction()
+        transactions.push(session.beginTransaction())
     }
 
     @Override
     void afterCompletion(WebRequest request, Exception ex) {
         super.afterCompletion(request, ex)
+        def transaction = transactions.pop()
         ex ? transaction.rollback() : transaction.commit()
+        log.debug "afterCompletion ${request.getDescription(true)}"
     }
-
-    /*@Override
-    void postHandle(WebRequest request, ModelMap model) {
-        super.postHandle(request, model)    //To change body of overridden methods use File | Settings | File Templates.
-    } */
-
 
 }
