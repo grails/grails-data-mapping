89fec39d90c4b7894ee5828610fd5de017c4aee1
Stefan Armbruster <stefan@armbruster-it.de>
2011-08-19 21:59:35 +0200

    neo4j: bumping version number for plugin

--- grails-plugins/neo4j/Neo4jGrailsPlugin.groovy
+++ grails-plugins/neo4j/Neo4jGrailsPlugin.groovy
@@ -11,7 +11,7 @@ class Neo4jGrailsPlugin {
     def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPNEO4J" ]
     def scm = [ url: "https://github.com/sarmbruster/grails-data-mapping" ]
 
-    def version = "0.9-SNAPSHOT"
+    def version = "1.0-SNAPSHOT"
     def grailsVersion = "1.2 > *"
     //def observe = ['services']
     //def loadAfter = ['domainClass', 'hibernate', 'services', 'cloudFoundry']
