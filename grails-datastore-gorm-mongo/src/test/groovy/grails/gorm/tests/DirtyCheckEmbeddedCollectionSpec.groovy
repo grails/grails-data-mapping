c3cde9d87eef1a339ad91c78ce6af5daf5ba3681
Graeme Rocher <graeme.rocher@gmail.com>
2011-10-11 14:50:00 +0200

    test for GPMONGODB-107 "Cannot empty embedded collections"

--- grails-datastore-gorm-mongo/src/test/groovy/grails/gorm/tests/DirtyCheckEmbeddedCollectionSpec.groovy
+++ grails-datastore-gorm-mongo/src/test/groovy/grails/gorm/tests/DirtyCheckEmbeddedCollectionSpec.groovy
@@ -13,14 +13,14 @@ import grails.persistence.Entity
 class DirtyCheckEmbeddedCollectionSpec extends GormDatastoreSpec {
 
     static {
-        TEST_CLASSES <<  Bar
+        TEST_CLASSES << Foo << Bar
     }
 
-    def "Test that changes to embedded collections are detected"() {
+    def "Test that changes to basic collections are detected"() {
         given:"A valid bar instance"
             def bar = createBar()
             session.clear()
-        when:"The an embedded collection is modified"
+        when:"A basic collection is modified"
             bar = Bar.get(bar.id)
             bar.strings.add("hello")
 		    bar.save(flush:true)
@@ -28,6 +28,41 @@ class DirtyCheckEmbeddedCollectionSpec extends GormDatastoreSpec {
             bar = Bar.get(bar.id)
         then:"The changes are reflected correctly in the persisted instance"
             bar.strings.size() == 3
+
+         when:"A basic collection is cleared"
+            bar.strings.clear()
+            bar.save(flush:true)
+            session.clear()
+            bar = Bar.get(bar.id)
+
+        then:"The collection is empty"
+            bar.strings.size() == 0
+
+    }
+
+    def "Test that an embedded collection can be cleared"() {
+        given:"valid foo and bar instances"
+            def foo = createFooBar()
+            session.clear()
+
+
+        when:"foo is looked up"
+            foo == Foo.get(foo.id)
+
+        then:"It has 1 bar"
+            foo.bars.size() == 1
+
+
+        when:"The collection is cleared"
+            foo.bars.clear()
+            foo.save(flush:true)
+            session.clear()
+
+            foo == Foo.get(foo.id)
+        then:"The collection is empty on nexted lookup"
+            foo.bars.size() == 0
+
+
     }
 
     protected def createBar() {
@@ -39,8 +74,23 @@ class DirtyCheckEmbeddedCollectionSpec extends GormDatastoreSpec {
         bar.strings.add("test2")
         bar.save(flush:true)
     }
+
+    protected createFooBar() {
+	    def bar = new Bar(foo:"test")
+		def foo = new Foo(testProperty:"test")
+		foo.bars.add(bar)
+		foo.save(flush:true)
+    }
 }
 
+@Entity
+class Foo {
+	ObjectId id
+	String testProperty
+    Set bars = []
+	static hasMany = [bars:Bar]
+	static embedded = ['bars']
+}
 @Entity
 class Bar {
 	ObjectId id
