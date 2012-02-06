7089fbb3cb09f217d1ff3dff846b916057886067
graemerocher <graeme.rocher@gmail.com>
2012-02-06 12:46:53 +0100

    fix for GPMONGODB-152 "Saving a duplicate id to the database does not fail (assigned ids)"

--- grails-datastore-gorm-mongo/src/test/groovy/org/grails/datastore/gorm/mongo/AssignedIdentifierSpec.groovy
+++ grails-datastore-gorm-mongo/src/test/groovy/org/grails/datastore/gorm/mongo/AssignedIdentifierSpec.groovy
@@ -3,6 +3,7 @@ package org.grails.datastore.gorm.mongo
 import grails.persistence.Entity
 import grails.gorm.tests.GormDatastoreSpec
 import spock.lang.Issue
+import org.springframework.dao.DataIntegrityViolationException
 
 /**
  * Tests for usage of assigned identifiers
@@ -38,6 +39,39 @@ class AssignedIdentifierSpec extends GormDatastoreSpec{
         then:"It is gone"
             River.count() == 0
             River.get("Amazon") == null
+    }
+    
+    @Issue("GPMONGODB-152")
+    void "Test that saving a second object with an assigned identifier produces an error"() {
+        when:"An entity is saved with an assigned id"
+            def r = new River(name:"Amazon", country: "Brazil")
+            r.save flush:true
+            session.clear()
+            r = River.get("Amazon")
+
+        then:"The entity can be retrieved"
+            r != null
+            r.name == "Amazon"
+            r.country == "Brazil"
+        
+        when:"A second object with the same id is saved"
+            r = new River(name:"Amazon", country: "Brazil")
+            r.save flush:true
+        
+        then:"An error is produced"
+            River.count() == 1
+            thrown DataIntegrityViolationException
+
+        when:"A new session is created"
+            def totalRivers = 0
+            River.withNewSession  {
+                r = new River(name:"Nile", country: "Egype")
+                r.save flush:true
+                totalRivers = River.count()
+            }
+
+        then:"It is possible to save new instances"
+            totalRivers == 2
     }
 
     @Issue("GPMONGODB-170")
