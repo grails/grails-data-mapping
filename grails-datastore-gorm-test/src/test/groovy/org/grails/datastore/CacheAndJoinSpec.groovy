3111b6e7655c074320fc0c3ae1b5b6007bfd9ae3
graemerocher <graeme.rocher@gmail.com>
2012-02-10 11:25:48 +0100

    fix for GRAILS-8758 "join() and cache() in criteria queries break unit tests"

---
+++ grails-datastore-gorm-test/src/test/groovy/org/grails/datastore/CacheAndJoinSpec.groovy
@@ -0,0 +1,59 @@
+package org.grails.datastore
+
+import grails.gorm.tests.GormDatastoreSpec
+import grails.persistence.Entity
+
+/**
+ *
+ */
+class CacheAndJoinSpec extends GormDatastoreSpec{
+
+    void "Test that the cache and join methods can be used in a test"() {
+        given:"Some test data"
+            new Author(name: "Bob").save flush:true
+            session.clear()
+        when:"The cache and join methods are used in criteria"
+            def a = Author.createCriteria().get {
+                eq 'name', "Bob"
+                join 'books'
+                maxResults 1
+                cache true
+            }
+        
+        then:"Results are returned"
+            a != null
+    }
+
+    @Override
+    List getDomainClasses() {
+        [Author, Book]
+    }
+
+
+}
+
+
+@Entity
+class Author {
+    Long id
+    String name
+
+    static hasMany = [books: Book]
+
+    static constraints = {
+        name blank: false
+    }
+}
+
+@Entity
+class Book {
+    Long id
+    String title
+
+    static belongsTo = [author: Author]
+
+    static constraints = {
+        title blank: false
+    }
+}
+
