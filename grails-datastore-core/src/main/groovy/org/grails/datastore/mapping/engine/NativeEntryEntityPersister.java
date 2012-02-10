394f8e7d96939c576c567e2b370fa424e57348d3
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-02-10 17:29:45 +0000

    Fix for GRAILS-8436
    Handles dirty check on custom types by reading the old value using the CustomTypeMarshaller and comparing that with the new value.

--- grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/engine/NativeEntryEntityPersister.java
+++ grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/engine/NativeEntryEntityPersister.java
@@ -1421,6 +1421,10 @@ public abstract class NativeEntryEntityPersister<T, K> extends LockableEntityPer
                 }
 
             }
+            else if (prop instanceof Custom) {
+                CustomTypeMarshaller marshaller = ((Custom)prop).getCustomTypeMarshaller();
+                return !areEqual(marshaller.read(prop, entry), currentValue, key);
+            }
             else {
                 throw new UnsupportedOperationException("dirty not detected for property " + prop.toString() + " " + prop.getClass().getSuperclass().toString());
             }
