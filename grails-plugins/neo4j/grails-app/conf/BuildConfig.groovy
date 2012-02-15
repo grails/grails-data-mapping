55ebdbcca7e3bfcdd453afcb3fb052d41101a94f
Stefan Armbruster <stefan@armbruster-it.de>
2012-02-15 01:10:09 +0100

    neo4j plugin: preparing for release of 1.0.0.M1
    - adopting BuildConfig.groovy from Mongo plugin
    - upgrading to use Grails 2.0.1 for building the plugin
    - bumping version number

--- grails-plugins/neo4j/grails-app/conf/BuildConfig.groovy
+++ grails-plugins/neo4j/grails-app/conf/BuildConfig.groovy
@@ -3,8 +3,12 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.dependency.resolution = {
-    inherits "global"
+
+    inherits("global") {
+        excludes 'xml-apis', 'netty'
+    }
     log "warn"
+
     repositories {
         grailsPlugins()
         grailsHome()
@@ -15,28 +19,30 @@ grails.project.dependency.resolution = {
         mavenRepo "http://repo.grails.org/grails/repo"
     }
 
-    plugins {
-        build(":release:1.0.0.RC3") {
-            export = false
-        }
-    }
-
     dependencies {
 
-        def version = "1.0.0.BUILD-SNAPSHOT"
-
-        compile("org.grails:grails-datastore-gorm-neo4j:$version",
-                "org.grails:grails-datastore-gorm-plugin-support:$version",        
-                "org.grails:grails-datastore-gorm:$version",
-                "org.grails:grails-datastore-core:$version",
-                "org.grails:grails-datastore-web:$version") {
+        def excludes = {
+            excludes "slf4j-simple", "persistence-api", "commons-logging", "jcl-over-slf4j", "slf4j-api", "jta"
+            excludes "spring-core", "spring-beans", "spring-aop", "spring-asm","spring-webmvc","spring-tx", "spring-context", "spring-web", "log4j", "slf4j-log4j12"
+            excludes group:"org.grails", name:'grails-core'
+            excludes group:"org.grails", name:'grails-gorm'
+            excludes group:"org.grails", name:'grails-test'
+            excludes group:'xml-apis', name:'xml-apis'
+            excludes 'ehcache-core'
             transitive = false
         }
 
-        test("org.grails:grails-datastore-gorm-test:$version",
-             "org.grails:grails-datastore-simple:$version"){
-            transitive = false
-        }
+        def datastoreVersion = "1.0.2.RELEASE"
+        def neo4jDatastoreVersion = "1.0.0.M11"
+
+        compile("org.grails:grails-datastore-gorm-neo4j:$neo4jDatastoreVersion",
+                "org.grails:grails-datastore-gorm-plugin-support:$datastoreVersion",
+                "org.grails:grails-datastore-gorm:$datastoreVersion",
+                "org.grails:grails-datastore-core:$datastoreVersion",
+                "org.grails:grails-datastore-web:$datastoreVersion", excludes)
+
+        test("org.grails:grails-datastore-gorm-test:$datastoreVersion",
+             "org.grails:grails-datastore-simple:$datastoreVersion", exlcudes)
 
         compile('org.neo4j:neo4j-community:1.6')
     }
