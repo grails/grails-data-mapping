700f5590dfbdb37560a1aa5d06251d55f0ea519a
Stefan Armbruster <stefan@armbruster-it.de>
2012-09-11 15:16:21 +0200

    use RestGraphDatabase together with a locally running server if sys property gorm_neo4j_test_use_rest is set to true

--- grails-datastore-gorm-neo4j/src/test/groovy/org/grails/datastore/gorm/Setup.groovy
+++ grails-datastore-gorm-neo4j/src/test/groovy/org/grails/datastore/gorm/Setup.groovy
@@ -1,8 +1,6 @@
 package org.grails.datastore.gorm
 
 import grails.gorm.tests.Role
-import grails.gorm.tests.User
-
 import org.codehaus.groovy.grails.commons.DefaultGrailsApplication
 import org.codehaus.groovy.grails.validation.ConstrainedProperty
 import org.codehaus.groovy.grails.validation.GrailsDomainClassValidator
@@ -11,36 +9,55 @@ import org.grails.datastore.gorm.events.DomainEventListener
 import org.grails.datastore.gorm.neo4j.Neo4jDatastore
 import org.grails.datastore.gorm.neo4j.Neo4jGormEnhancer
 import org.grails.datastore.gorm.neo4j.constraints.UniqueConstraint
-import org.springframework.context.support.GenericApplicationContext
 import org.grails.datastore.mapping.core.Session
 import org.grails.datastore.mapping.model.MappingContext
 import org.grails.datastore.mapping.model.PersistentEntity
 import org.grails.datastore.mapping.transactions.DatastoreTransactionManager
+import org.neo4j.test.ImpermanentGraphDatabase
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.context.support.GenericApplicationContext
 import org.springframework.util.StringUtils
 import org.springframework.validation.Errors
 import org.springframework.validation.Validator
-import org.slf4j.LoggerFactory
-import org.slf4j.Logger
-import org.neo4j.test.ImpermanentGraphDatabase
+import org.neo4j.rest.graphdb.RestGraphDatabase
+import org.neo4j.rest.graphdb.LocalTestServer
+import org.neo4j.rest.graphdb.util.Config
 
 class Setup {
 
+    static HOST = "localhost"
+    static PORT = 7473
     protected final Logger log = LoggerFactory.getLogger(getClass())
 
     static session
     static datastore
     static transaction
+    static server
 
     static destroy() {
         transaction.rollback()
         session.nativeInterface.shutdown()
+        server?.stop()
     }
 
     static Session setup(classes) {
 
         def ctx = new GenericApplicationContext()
         ctx.refresh()
-        datastore = new Neo4jDatastore(graphDatabaseService: new ImpermanentGraphDatabase(), applicationContext: ctx)
+
+        def graphDb;
+        if (System.properties.get("gorm_neo4j_test_use_rest")) {
+            System.setProperty(Config.CONFIG_BATCH_TRANSACTION,"false") // TODO: remove when support for batch has been finished
+            //System.setProperty(Config.CONFIG_LOG_REQUESTS,"true") // enable for verbose request/response logging
+            server = new LocalTestServer(HOST, PORT).withPropertiesFile("neo4j-server.properties");
+            server.start()
+            graphDb = new RestGraphDatabase("http://localhost:7473/db/data/")
+        } else {
+            graphDb = new ImpermanentGraphDatabase()
+        }
+
+        datastore = new Neo4jDatastore(graphDatabaseService: graphDb, applicationContext: ctx)
 
         /*Neo4jSession.metaClass.invokeMethod = { String name, args ->
             def metaMethod = Neo4jSession.metaClass.getMetaMethod(name, args)
