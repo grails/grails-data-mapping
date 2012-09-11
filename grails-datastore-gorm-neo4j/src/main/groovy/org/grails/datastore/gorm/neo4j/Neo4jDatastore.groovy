3d049c1e1ced3ec556303635ce3e91171f8ff7c8
Stefan Armbruster <stefan@armbruster-it.de>
2012-09-11 15:17:52 +0200

    support for autoindexing in case of type=="rest"

--- grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/Neo4jDatastore.groovy
+++ grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/Neo4jDatastore.groovy
@@ -27,7 +27,6 @@ import org.neo4j.graphdb.index.IndexManager
 import org.grails.datastore.mapping.model.PersistentProperty
 import org.neo4j.graphdb.index.AutoIndexer
 import org.grails.datastore.mapping.model.types.Simple
-import org.neo4j.kernel.AbstractGraphDatabase
 
 /**
  * Datastore implementation for Neo4j backend
@@ -96,20 +95,18 @@ class Neo4jDatastore extends AbstractDatastore implements InitializingBean {
     }
 
     protected void setupIndexing() {
-        if (graphDatabaseService instanceof AbstractGraphDatabase) {
-            AutoIndexer<Node> nodeAutoIndex = indexManager.nodeAutoIndexer
-            nodeAutoIndex.enabled = true
-            nodeAutoIndex.startAutoIndexingProperty(Neo4jSession.TYPE_PROPERTY_NAME)
-            for (PersistentEntity pe in mappingContext.persistentEntities) {
+        AutoIndexer<Node> nodeAutoIndex = indexManager.nodeAutoIndexer
+        nodeAutoIndex.enabled = true
+        nodeAutoIndex.startAutoIndexingProperty(Neo4jSession.TYPE_PROPERTY_NAME)
+        for (PersistentEntity pe in mappingContext.persistentEntities) {
 
-                for (PersistentEntity parent in collectSuperclassChain(pe)) {
-                    domainSubclasses[parent] << pe
-                }
+            for (PersistentEntity parent in collectSuperclassChain(pe)) {
+                domainSubclasses[parent] << pe
+            }
 
-                for (PersistentProperty pp in pe.persistentProperties) {
-                    if ((pp instanceof Simple) && (pp.mapping.mappedForm.index)) {
-                        nodeAutoIndex.startAutoIndexingProperty(pp.name)
-                    }
+            for (PersistentProperty pp in pe.persistentProperties) {
+                if ((pp instanceof Simple) && (pp.mapping.mappedForm.index)) {
+                    nodeAutoIndex.startAutoIndexingProperty(pp.name)
                 }
             }
         }
