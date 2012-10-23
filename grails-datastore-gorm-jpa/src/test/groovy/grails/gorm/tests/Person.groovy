65ef1e2909b8976ec76b8f628408a01a47421a6e
Graeme Rocher <graeme.rocher@gmail.com>
2012-10-23 12:45:38 +0200

    fix failing tests

--- grails-datastore-gorm-jpa/src/test/groovy/grails/gorm/tests/Person.groovy
+++ grails-datastore-gorm-jpa/src/test/groovy/grails/gorm/tests/Person.groovy
@@ -9,14 +9,6 @@ class Person implements Serializable {
     Integer age = 0
     static hasMany = [pets:Pet]
 
-    static peopleWithOlderPets = where {
-        pets {
-            age > 9
-        }
-    }
-    static peopleWithOlderPets2 = where {
-        pets.age > 9
-    }
     static mapping = {
         firstName index:true
         lastName index:true
