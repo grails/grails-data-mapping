141f7886e4f867e4846d0754d36bccce3bc99a7f
Roman Stepanenko <rs.opensource@gmail.com>
2012-11-05 08:50:46 -0500

    getting ready to release version 0.5, upgraded AWS SDK to 1.3.23

--- grails-plugins/simpledb/grails-app/conf/BuildConfig.groovy
+++ grails-plugins/simpledb/grails-app/conf/BuildConfig.groovy
@@ -8,14 +8,23 @@ grails.project.dependency.resolution = {
 
     log "warn"
 
-    String simpledbVersion = "0.5.BUILD-SNAPSHOT"
+    String simpledbVersion = "0.5"
     //for local development and testing of the plugin:
     // 1) change version in grails-data-mapping/build.gradle to an appropriate snapshot
     // 2) grails-data-mapping/gradle install
     // 3) specify the same snapshot version here in the line below after the comments
     // 4) in your grails app BuildConfig: grails.plugin.location.'simpledb' = "C:/Source/grails-data-mapping/grails-plugins/simpledb"
     // 5) in your grails app BuildConfig: enable mavenLocal() in repositories AND put it first in the list of repos
-//    String simpledbVersion = "0.2.BUILD-SNAPSHOT"
+
+    // to publish the version of the plugin:
+    // change version from '0.x.BUILD-SNAPSHOT' to '0.x' in the following files:
+    // grails-plugins\simpledb\SimpledbGrailsPlugin.groovy
+    // grails-plugins\simpledb\grails-app\conf\BuildConfig.groovy
+    // grails-datastore-gorm-simpledb\build.gradle
+    // grails-datastore-simpledb\build.gradle
+
+    // commit, publish, after release increment the version and use BUILD-SNAPSHOT and commit again
+
 
     String datastoreVersion = "1.0.0.RELEASE"
 
@@ -44,7 +53,7 @@ grails.project.dependency.resolution = {
          }        
 
         runtime("stax:stax:1.2.0", excludes)
-        runtime('com.amazonaws:aws-java-sdk:1.3.14')
+        runtime('com.amazonaws:aws-java-sdk:1.3.23')
 
         test("org.grails:grails-datastore-gorm-test:$datastoreVersion",
              "org.grails:grails-datastore-simple:$datastoreVersion") {
