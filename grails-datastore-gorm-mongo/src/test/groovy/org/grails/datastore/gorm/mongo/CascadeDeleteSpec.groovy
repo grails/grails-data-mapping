ddd65af96d4f43e6cd8c0fede1804839334c3124
Graeme Rocher <graeme.rocher@gmail.com>
2012-12-17 11:56:08 +0100

    test for GPMONGODB-187

---
+++ grails-datastore-gorm-mongo/src/test/groovy/org/grails/datastore/gorm/mongo/CascadeDeleteSpec.groovy
@@ -0,0 +1,70 @@
+package org.grails.datastore.gorm.mongo
+
+import spock.lang.Specification
+import grails.gorm.tests.GormDatastoreSpec
+import org.bson.types.ObjectId
+import grails.persistence.Entity
+import spock.lang.Issue
+
+/**
+ */
+class CascadeDeleteSpec extends GormDatastoreSpec{
+
+    @Issue('GPMONGODB-187')
+    void "Test that a delete cascade from owner to child"() {
+        given:"An owner with a child object "
+            def u = new CascadeUser(name:"user2")
+            def s = new CascadeUserSettings()
+            u.settings = [s] as Set
+
+            u.save(flush:true)
+
+        when:"The owner is queried"
+            def found1 = CascadeUser.findByName("user2")
+            def found1a = CascadeUserSettings.findByUser(found1)
+
+
+        then:"The data is correct"
+            found1 != null
+            found1.settings.size()  == 1
+
+
+        when:"The owner is deleted"
+            found1.delete(flush:true)
+            def found2 = CascadeUser.findByName("user2")
+            def found1b = CascadeUserSettings.findByUser(found1)
+
+        then:"So is the child"
+            found2 == null
+            found1b == null
+    }
+
+    @Override
+    List getDomainClasses() {
+        [CascadeUser, CascadeUserSettings]
+    }
+}
+
+@Entity
+class CascadeUser {
+
+    ObjectId id
+
+    String name
+
+    Set<CascadeUserSettings> settings
+    static hasMany = [settings:CascadeUserSettings]
+
+    static constraints = {
+    }
+}
+@Entity
+class CascadeUserSettings {
+
+    ObjectId id
+
+    boolean someSetting = true
+
+    static belongsTo = [user:CascadeUser]
+
+}
