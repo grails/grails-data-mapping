25d70d99488ad9b9a349b944231bca1f4e8f16d1
Jeff Brown <jbrown@vmware.com>
2013-01-03 12:28:36 -0600

    redis does not currently support IsNull queries

---
+++ grails-datastore-gorm-redis/src/test/groovy/grails/gorm/tests/QueryByNullSpec.groovy
@@ -0,0 +1,9 @@
+package grails.gorm.tests
+
+class QueryByNullSpec extends GormDatastoreSpec {
+    @spock.lang.Ignore
+    void 'Test passing null as the sole argument to a dynamic finder multiple times'() {
+        // see GRAILS-3463
+    }
+}
+
