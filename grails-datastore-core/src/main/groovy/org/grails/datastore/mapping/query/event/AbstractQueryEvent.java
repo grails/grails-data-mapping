ede72c9bccae1e56d13a308d299e46b9f627b59a
Tom Widmer <tom.widmer@camcog.com>
2013-06-09 21:52:58 +0100

    GPMONGODB-300 Add query events, implementation and test cases

---
+++ grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/query/event/AbstractQueryEvent.java
@@ -0,0 +1,24 @@
+package org.grails.datastore.mapping.query.event;
+
+import org.grails.datastore.mapping.query.Query;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * Base class for query events.
+ */
+public abstract class AbstractQueryEvent extends ApplicationEvent {
+    /**
+     * The query.
+     */
+    protected Query query;
+
+    public AbstractQueryEvent(Query query) {
+        super(query.getSession().getDatastore());
+        this.query = query;
+    }
+
+    /**
+     * @return The type of event.
+     */
+    public abstract QueryEventType getEventType();
+}
---
+++ grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/query/event/PreQueryEvent.java
@@ -0,0 +1,20 @@
+package org.grails.datastore.mapping.query.event;
+
+import org.grails.datastore.mapping.query.Query;
+
+/**
+ * Event fired immediately before a query is executed.
+ */
+public class PreQueryEvent extends AbstractQueryEvent {
+    public PreQueryEvent(Query query) {
+        super(query);
+    }
+
+    /**
+     * @return The type of event.
+     */
+    @Override
+    public QueryEventType getEventType() {
+        return QueryEventType.PreExecution;
+    }
+}
