5f37225c134b363071222c07d703e089b12d9273
Stefan Armbruster <stefan@armbruster-it.de>
2013-08-05 15:25:39 +0200

    fix withManualFlushMode

--- grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/validation/constraints/UniqueConstraint.groovy
+++ grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/validation/constraints/UniqueConstraint.groovy
@@ -124,8 +124,8 @@ class UniqueConstraint extends AbstractConstraint {
             final flushMode = session.getFlushMode()
 
             try {
-                callable.call(session)
                 session.setFlushMode(javax.persistence.FlushModeType.COMMIT)
+                callable.call(session)
             } finally {
                 session.setFlushMode(flushMode)
             }
