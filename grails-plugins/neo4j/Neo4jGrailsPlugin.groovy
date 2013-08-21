cf64694edc69eae77f03ce928830ca41bf7c9e15
Stefan Armbruster <stefan@armbruster-it.de>
2013-08-21 22:07:59 +0200

    neo4j: bumping version number for release 1.0.1

--- grails-plugins/neo4j/Neo4jGrailsPlugin.groovy
+++ grails-plugins/neo4j/Neo4jGrailsPlugin.groovy
@@ -19,8 +19,8 @@ class Neo4jGrailsPlugin {
     def issueManagement = [ system: "JIRA", url: "https://github.com/SpringSource/grails-data-mapping/issues" ]
     def scm = [ url: "https://github.com/sarmbruster/grails-data-mapping" ]
 
-    //def version = "1.0.1"
-    def version = "1.0.1-SNAPSHOT"
+    def version = "1.0.1"
+//    def version = "1.0.2-SNAPSHOT"
     def grailsVersion = "1.2 > *"
     def loadAfter = ['domainClass', 'hibernate', 'services', 'cloudFoundry', 'converters']
     def observe = ['services', 'domainClass']
