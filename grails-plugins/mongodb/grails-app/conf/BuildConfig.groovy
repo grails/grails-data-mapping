0ab812a6fae30130b3d1fa9c5b23069f86c8638a
Graeme Rocher <graeme.rocher@gmail.com>
2013-11-13 12:35:00 +0100

    update to 2.0.0. of release plugin

--- grails-plugins/mongodb/grails-app/conf/BuildConfig.groovy
+++ grails-plugins/mongodb/grails-app/conf/BuildConfig.groovy
@@ -51,7 +51,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(':release:3.0.1', ':rest-client-builder:1.0.3') {
+        build(':release:3.0.1', ':rest-client-builder:2.0.0') {
             export = false
         }
     }
