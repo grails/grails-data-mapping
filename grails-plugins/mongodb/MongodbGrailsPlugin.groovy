7ea80e504a75eecbe97e9085ac281db5c4eeeecd
Graeme Rocher <graeme.rocher@gmail.com>
2014-04-23 11:22:55 +0200

    update mongodb plugin version for 3.0 release

--- grails-plugins/mongodb/MongodbGrailsPlugin.groovy
+++ grails-plugins/mongodb/MongodbGrailsPlugin.groovy
@@ -15,7 +15,7 @@ class MongodbGrailsPlugin {
     def issueManagement = [system: "JIRA", url: "http://jira.grails.org/browse/GPMONGODB"]
     def scm = [url: "https://github.com/grails/grails-data-mapping"]
 
-    def version = "2.0.1"
+    def version = "3.0.0"
     def grailsVersion = "2.3.2 > *"
     def observe = ['services', 'domainClass']
     def loadAfter = ['domainClass', 'hibernate', 'hibernate4', 'services', 'cloudFoundry']
--- grails-plugins/mongodb/grails-app/conf/BuildConfig.groovy
+++ grails-plugins/mongodb/grails-app/conf/BuildConfig.groovy
@@ -29,8 +29,8 @@ grails.project.dependency.resolution = {
 
 
 
-        def datastoreVersion = "3.0.4.RELEASE"
-        def mongoDatastoreVersion = "2.0.1.RELEASE"
+        def datastoreVersion = "3.0.6.RELEASE"
+        def mongoDatastoreVersion = "3.0.0.RELEASE"
 
         compile ("org.grails:grails-datastore-gorm-mongodb:$mongoDatastoreVersion",excludes)
         compile("org.grails:grails-datastore-gorm-plugin-support:$datastoreVersion",
