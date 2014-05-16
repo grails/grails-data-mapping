9036cf61053c4d2348e80df0e2739c27e2f10210
Graeme Rocher <graeme.rocher@gmail.com>
2014-05-16 18:02:08 +0200

    correctly dedect the exit code for all suites

--- travis-build.sh
+++ travis-build.sh
@@ -1,7 +1,12 @@
 export GRADLE_OPTS="-Xmx2048m -Xms256m -XX:MaxPermSize=768m -XX:+CMSClassUnloadingEnabled -XX:+HeapDumpOnOutOfMemoryError" 
 
 
-./gradlew grails-datastore-gorm-hibernate:test
-./gradlew grails-datastore-gorm-hibernate4:test
-./gradlew grails-datastore-gorm-mongodb:test
-./gradlew grails-datastore-gorm-test:test
+EXIT_STATUS=0
+./gradlew grails-datastore-gorm-hibernate:test || EXIT_STATUS=$?
+./gradlew grails-datastore-gorm-hibernate4:test || EXIT_STATUS=$?
+./gradlew grails-datastore-gorm-mongodb:test || EXIT_STATUS=$?
+./gradlew grails-datastore-gorm-test:test || EXIT_STATUS=$?
+exit $EXIT_STATUS
+
+
+
