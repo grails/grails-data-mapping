46db578566eef1875dd5bd3ef694e4be730a3842
Paras Lakhani <paras@atoms.to>
2014-05-18 17:26:12 +0100

    custom GORM instance and static api creation and methods to allow params to be passed to Query

--- grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/gorm/cassandra/plugin/support/CassandraMethodsConfigurer.groovy
+++ grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/gorm/cassandra/plugin/support/CassandraMethodsConfigurer.groovy
@@ -1,17 +1,53 @@
 package org.grails.datastore.gorm.cassandra.plugin.support
 
+import org.grails.datastore.gorm.GormEnhancer
+import org.grails.datastore.gorm.GormInstanceApi
+import org.grails.datastore.gorm.GormStaticApi
+import org.grails.datastore.gorm.cassandra.CassandraGormEnhancer
+import org.grails.datastore.gorm.cassandra.CassandraGormInstanceApi
+import org.grails.datastore.gorm.cassandra.CassandraGormStaticApi
+import org.grails.datastore.gorm.finders.FinderMethod
 import org.grails.datastore.gorm.plugin.support.DynamicMethodsConfigurer
-import org.grails.datastore.mapping.core.Datastore;
+import org.grails.datastore.mapping.core.Datastore
 import org.springframework.transaction.PlatformTransactionManager
 
 class CassandraMethodsConfigurer extends DynamicMethodsConfigurer {
 
-	CassandraMethodsConfigurer(Datastore datastore, PlatformTransactionManager transactionManager) {
-		super(datastore, transactionManager);
-	}
+    CassandraMethodsConfigurer(Datastore datastore) {
+        this(datastore, null)
+    }
+        
+    CassandraMethodsConfigurer(Datastore datastore, PlatformTransactionManager transactionManager) {
+        super(datastore, transactionManager)
+    }
 
-	@Override
-	String getDatastoreType() {
-		return "Cassandra";
-	}
+    @Override
+    String getDatastoreType() {
+        return "Cassandra"
+    }
+    
+    @Override
+    protected GormStaticApi createGormStaticApi(Class cls, List<FinderMethod> finders) {
+        return new CassandraGormStaticApi(cls, datastore, finders)
+    }
+    
+    @Override
+    protected GormInstanceApi createGormInstanceApi(Class cls) {
+        final api = new CassandraGormInstanceApi(cls, datastore)
+        api.failOnError = failOnError
+        return api
+    }
+    
+    @Override
+    protected GormEnhancer createEnhancer() {
+        def ge
+        if (transactionManager == null) {
+            ge = new CassandraGormEnhancer(datastore)
+        }
+        else {
+            ge = new CassandraGormEnhancer(datastore, transactionManager)
+        }
+        ge.failOnError = failOnError
+        ge
+    }
 }
