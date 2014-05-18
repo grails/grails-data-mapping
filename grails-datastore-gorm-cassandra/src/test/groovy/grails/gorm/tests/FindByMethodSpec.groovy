07bbd401aca97750dbf198e18841717f2c3187c5
Paras Lakhani <paras@atoms.to>
2014-05-18 17:36:00 +0100

    changed to get working against implementation

--- grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/FindByMethodSpec.groovy
+++ grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/FindByMethodSpec.groovy
@@ -1,16 +1,19 @@
 package grails.gorm.tests
 
+import com.datastax.driver.core.exceptions.InvalidQueryException
+
+
 /**
  * @author graemerocher
  */
 class FindByMethodSpec extends GormDatastoreSpec {
-
+    
     void 'Test Using AND Multiple Times In A Dynamic Finder'() {
         given:
-            new Person(firstName: 'Jake', lastName: 'Brown', age: 11).save()
-            new Person(firstName: 'Zack', lastName: 'Brown', age: 14).save()
-            new Person(firstName: 'Jeff', lastName: 'Brown', age: 41).save()
-            new Person(firstName: 'Zack', lastName: 'Galifianakis', age: 41).save()
+            new Person(firstName: 'Jake', lastName: 'Brown', age: 11).insert()
+            new Person(firstName: 'Zack', lastName: 'Brown', age: 14).insert()
+            new Person(firstName: 'Jeff', lastName: 'Brown', age: 41).insert()
+            new Person(firstName: 'Zack', lastName: 'Galifianakis', age: 41).insert()
 
         when:
             def people = Person.findAllByFirstNameAndLastNameAndAge('Jeff', 'Brown', 1)
@@ -32,6 +35,38 @@ class FindByMethodSpec extends GormDatastoreSpec {
             14 == people[0].age
 
         when:
+            people = Person.findAllByFirstNameAndLastNameAndAgeLessThan('Jake', 'Brown', 14)
+
+        then:
+            1 == people?.size()
+            11 == people[0].age
+        
+        when:
+            people = Person.findAllByLastNameAndAgeLessThanEquals('Brown', 14, [allowFiltering:true])
+
+        then:
+            2 == people?.size()
+            11 == people[0].age
+            14 == people[1].age
+        
+        when:
+            people = Person.findAllByLastNameAndAgeGreaterThanEquals('Brown', 14, [allowFiltering:true])
+
+        then:
+            2 == people?.size()
+            14 == people[0].age
+            41 == people[1].age
+        
+        when:
+            people = Person.findAllByLastNameAndAgeBetween('Brown', 11, 42, [allowFiltering:true])
+
+        then:
+            3 == people?.size()
+            11 == people[0].age
+            14 == people[1].age
+            41 == people[2].age
+            
+        when:
             def cnt = Person.countByFirstNameAndLastNameAndAge('Jake', 'Brown', 11)
 
         then:
@@ -44,7 +79,7 @@ class FindByMethodSpec extends GormDatastoreSpec {
             1 == cnt
     }
 
-    void 'Test Using OR Multiple Times In A Dynamic Finder'() {
+    void 'Test Using OR exception In A Dynamic Finder'() {
         given:
             new Person(firstName: 'Jake', lastName: 'Brown', age: 11).save()
             new Person(firstName: 'Zack', lastName: 'Brown', age: 14).save()
@@ -55,19 +90,7 @@ class FindByMethodSpec extends GormDatastoreSpec {
             def people = Person.findAllByFirstNameOrLastNameOrAge('Zack', 'Tyler', 125)
 
         then:
-            2 == people?.size()
-
-        when:
-            people = Person.findAllByFirstNameOrLastNameOrAge('Zack', 'Brown', 125)
-
-        then:
-            4 == people?.size()
-
-        when:
-            def cnt = Person.countByFirstNameOrLastNameOrAgeInList('Jeff', 'Wilson', [11, 41])
-
-        then:
-            3 == cnt
+           thrown UnsupportedOperationException      
     }
 
     void testBooleanPropertyQuery() {
@@ -78,13 +101,13 @@ class FindByMethodSpec extends GormDatastoreSpec {
             new Highway(bypassed: false, name: 'Not Bypassed Highway').save()
 
         when:
-            def highways= Highway.findAllBypassedByName('Not Bypassed Highway')
+            def highways= Highway.findAllBypassedByName('Not Bypassed Highway', [allowFiltering:true])
 
         then:
             0 == highways.size()
 
         when:
-            highways = Highway.findAllNotBypassedByName('Not Bypassed Highway')
+            highways = Highway.findAllNotBypassedByName('Not Bypassed Highway', [allowFiltering:true])
 
         then:
             2 == highways?.size()
@@ -92,7 +115,7 @@ class FindByMethodSpec extends GormDatastoreSpec {
             'Not Bypassed Highway'== highways[1].name
 
         when:
-            highways = Highway.findAllBypassedByName('Bypassed Highway')
+            highways = Highway.findAllBypassedByName('Bypassed Highway', [allowFiltering:true])
 
         then:
             2 == highways?.size()
@@ -100,7 +123,7 @@ class FindByMethodSpec extends GormDatastoreSpec {
             'Bypassed Highway'== highways[1].name
 
         when:
-            highways = Highway.findAllNotBypassedByName('Bypassed Highway')
+            highways = Highway.findAllNotBypassedByName('Bypassed Highway', [allowFiltering:true])
         then:
             0 == highways?.size()
 
@@ -129,12 +152,12 @@ class FindByMethodSpec extends GormDatastoreSpec {
             'Bypassed Highway' == highway?.name
 
         when:
-            highway = Highway.findNotBypassedByName('Not Bypassed Highway')
+            highway = Highway.findNotBypassedByName('Not Bypassed Highway', [allowFiltering:true])
         then:
             'Not Bypassed Highway' == highway?.name
 
         when:
-            highway = Highway.findBypassedByName('Bypassed Highway')
+            highway = Highway.findBypassedByName('Bypassed Highway', [allowFiltering:true])
         then:
             'Bypassed Highway' == highway?.name
 
@@ -169,21 +192,44 @@ class FindByMethodSpec extends GormDatastoreSpec {
         when:
             book = Book.findPublishedByTitleOrAuthor('Fly Fishing For Everyone', 'Dierk')
         then:
-            'GINA'== book.title
-            Book.findPublished() != null
-
+            thrown UnsupportedOperationException 
+            
+        when:
+            book = Book.findByTitle('DGGv2')
+        then:
+            'Jeff'== book.author
+            'DGGv2'== book.title
+            true == book.published
+        
+        when:
+            book = Book.findByTitle('None')
+        then:
+            book == null
+        
+        when: 
+            book = Book.findByAuthorAndTitle('Jeff', 'DGGv2')
+        then:
+            'Jeff'== book.author
+            'DGGv2'== book.title
+            true == book.published
+        
+        when:
+            book = Book.findByAuthorAndTitle('Jeff', 'None')
+        then:
+            book == null
+            
         when:
             book = Book.findNotPublished()
         then:
             'Fly Fishing For Everyone' == book?.title
-
+        /* throws ReadTimeoutException, kind of expected
         when:
-            def books = Book.findAllPublishedByTitle('DGGv2')
+            def books = Book.findAllByTitlePublished('DGGv2', [allowFiltering:true])
         then:
             2 == books?.size()
-
+        */
         when:
-            books = Book.findAllPublished()
+            def books = Book.findAllPublished()
         then:
             3 == books?.size()
 
@@ -193,6 +239,21 @@ class FindByMethodSpec extends GormDatastoreSpec {
             1 == books?.size()
 
         when:
+            books = Book.findAllByAuthor('Jeff')
+        then:
+            2 == books?.size()
+        
+        when:
+            books = Book.findAllByAuthorAndTitle('Jeff', 'DGGv2')
+        then:
+            1 == books?.size()
+        
+        when:
+            books = Book.findAllByTitle('Fly Fishing For Everyone')
+        then:
+            1 == books?.size()
+            
+        when:
             books = Book.findAllPublishedByTitleAndAuthor('DGGv2', 'Graeme')
         then:
             1 == books?.size()
@@ -200,7 +261,7 @@ class FindByMethodSpec extends GormDatastoreSpec {
         when:
             books = Book.findAllPublishedByAuthorOrTitle('Graeme', 'GINA')
         then:
-            2 == books?.size()
+            thrown UnsupportedOperationException 
 
         when:
             books = Book.findAllNotPublishedByAuthor('Jeff')
@@ -216,21 +277,27 @@ class FindByMethodSpec extends GormDatastoreSpec {
     void "Test findOrCreateBy For A Record That Does Not Exist In The Database"() {
         when:
             def book = Book.findOrCreateByAuthor('Someone')
-
+            def highway = Highway.findOrCreateByName('Name')
         then:
             'Someone' == book.author
             null == book.title
-            null == book.id
+                   
+            'Name' == highway.name
+            null == highway.other
+            null == highway.id        
     }
 
     void "Test findOrCreateBy With An AND Clause"() {
         when:
             def book = Book.findOrCreateByAuthorAndTitle('Someone', 'Something')
-
+            def highway = Highway.findOrCreateByNameAndOther('Name', 'Other', [allowFiltering:true])
         then:
             'Someone' == book.author
             'Something' == book.title
-            null == book.id
+            
+            'Name' == highway.name
+            'Other' == highway.other
+            null == highway.id
     }
 
     void "Test findOrCreateBy Throws Exception If An OR Clause Is Used"() {
@@ -244,34 +311,37 @@ class FindByMethodSpec extends GormDatastoreSpec {
     void "Test findOrSaveBy For A Record That Does Not Exist In The Database"() {
         when:
             def book = Book.findOrSaveByAuthorAndTitle('Some New Author', 'Some New Title')
-
+            def highway = Highway.findOrSaveByNameAndOther('Name', 'Other', [allowFiltering:true])
         then:
             'Some New Author' == book.author
             'Some New Title' == book.title
-            book.id != null
+            
+            'Name' == highway.name
+            'Other' == highway.other
+            null != highway.id
     }
 
     void "Test findOrSaveBy For A Record That Does Exist In The Database"() {
 
         given:
-            def originalId = new Book(author: 'Some Author', title: 'Some Title').save().id
+            def originalId = new Highway(name: 'Name', other: 'Other').save().id
 
         when:
-            def book = Book.findOrSaveByAuthor('Some Author')
+            def highway = Highway.findOrSaveByName('Name')
 
         then:
-            'Some Author' == book.author
-            'Some Title' == book.title
-            originalId == book.id
+            'Name' == highway.name
+            'Other' == highway.other
+            originalId == highway.id
     }
 
     void "Test patterns which shold throw MissingMethodException"() {
             // Redis doesn't like Like queries...
-//        when:
-//            Book.findOrCreateByAuthorLike('B%')
-//
-//        then:
-//            thrown MissingMethodException
+        when:
+            Book.findOrCreateByAuthorLike('B%')
+
+        then:
+            thrown UnsupportedOperationException
 
         when:
             Book.findOrCreateByAuthorInList(['Jeff'])
@@ -289,69 +359,68 @@ class FindByMethodSpec extends GormDatastoreSpec {
             Book.findOrCreateByAuthorNotEqual('B')
 
         then:
-            thrown MissingMethodException
+            thrown UnsupportedOperationException
 
         when:
             Book.findOrCreateByAuthorGreaterThan('B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
         when:
             Book.findOrCreateByAuthorLessThan('B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
         when:
             Book.findOrCreateByAuthorBetween('A', 'B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
         when:
             Book.findOrCreateByAuthorGreaterThanEquals('B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
         when:
             Book.findOrCreateByAuthorLessThanEquals('B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
             // GemFire doesn't like these...
-//        when:
-//            Book.findOrCreateByAuthorIlike('B%')
-//
-//        then:
-//            thrown MissingMethodException
+        when:
+            Book.findOrCreateByAuthorIlike('B%')
 
-//        when:
-//            Book.findOrCreateByAuthorRlike('B%')
-//
-//        then:
-//            thrown MissingMethodException
+        then:
+            thrown UnsupportedOperationException
 
-//        when:
-//            Book.findOrCreateByAuthorIsNull()
-//
-//        then:
-//            thrown MissingMethodException
+        when:
+            Book.findOrCreateByAuthorRlike('B%')
 
-//        when:
-//            Book.findOrCreateByAuthorIsNotNull()
-//
-//        then:
-//            thrown MissingMethodException
+        then:
+            thrown UnsupportedOperationException
 
-            // Redis doesn't like Like queries...
-//        when:
-//            Book.findOrSaveByAuthorLike('B%')
-//
-//        then:
-//            thrown MissingMethodException
+        when:
+            Book.findOrCreateByAuthorIsNull()
+
+        then:
+            thrown UnsupportedOperationException
+
+        when:
+            Book.findOrCreateByAuthorIsNotNull()
+
+        then:
+            thrown UnsupportedOperationException
+
+        when:
+            Book.findOrSaveByAuthorLike('B%')
+
+        then:
+            thrown UnsupportedOperationException
 
         when:
             Book.findOrSaveByAuthorInList(['Jeff'])
@@ -369,61 +438,60 @@ class FindByMethodSpec extends GormDatastoreSpec {
             Book.findOrSaveByAuthorNotEqual('B')
 
         then:
-            thrown MissingMethodException
+            thrown UnsupportedOperationException
 
         when:
             Book.findOrSaveByAuthorGreaterThan('B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
         when:
             Book.findOrSaveByAuthorLessThan('B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
         when:
             Book.findOrSaveByAuthorBetween('A', 'B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
         when:
             Book.findOrSaveByAuthorGreaterThanEquals('B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
         when:
             Book.findOrSaveByAuthorLessThanEquals('B')
 
         then:
-            thrown MissingMethodException
+            thrown InvalidQueryException
 
-            // GemFire doesn't like these...
-//        when:
-//            Book.findOrSaveByAuthorIlike('B%')
-//
-//        then:
-//            thrown MissingMethodException
+        when:
+            Book.findOrSaveByAuthorIlike('B%')
 
-//        when:
-//            Book.findOrSaveByAuthorRlike('B%')
-//
-//        then:
-//            thrown MissingMethodException
+        then:
+            thrown UnsupportedOperationException
 
-//        when:
-//            Book.findOrSaveByAuthorIsNull()
-//
-//        then:
-//            thrown MissingMethodException
+        when:
+            Book.findOrSaveByAuthorRlike('B%')
 
-//        when:
-//            Book.findOrSaveByAuthorIsNotNull()
-//
-//        then:
-//            thrown MissingMethodException
+        then:
+            thrown UnsupportedOperationException
+
+        when:
+            Book.findOrSaveByAuthorIsNull()
+
+        then:
+            thrown UnsupportedOperationException
+
+        when:
+            Book.findOrSaveByAuthorIsNotNull()
+
+        then:
+            thrown UnsupportedOperationException
     }
 }
