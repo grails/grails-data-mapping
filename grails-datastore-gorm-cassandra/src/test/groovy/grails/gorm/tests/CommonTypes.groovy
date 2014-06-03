f2c8f386865ecba09df2b6f60a2c78a3f8d0d6a5
Paras Lakhani <paras@atoms.to>
2014-06-03 13:49:37 +0100

    enable as cassandra entity

--- grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/CommonTypes.groovy
+++ grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/CommonTypes.groovy
@@ -1,25 +1,10 @@
 package grails.gorm.tests;
 
-import java.io.Serializable;
-import java.net.URL;
-import java.util.Calendar;
-import java.util.Currency;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
-import java.util.UUID;
+import grails.gorm.CassandraEntity
 
-import grails.persistence.Entity
-
-@Entity
+@CassandraEntity
 class CommonTypes implements Serializable {
-    UUID id
-    Long version
-    Long l
-    Byte b
-    Short s
-    Boolean bool
-    Integer i
+    
     URL url
     Date date
     Calendar c
