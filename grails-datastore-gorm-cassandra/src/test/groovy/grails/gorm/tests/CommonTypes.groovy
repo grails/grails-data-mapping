eee1393af7d271c66c4a61f8af755d83d40c6564
Paras Lakhani <paras@atoms.to>
2014-06-04 16:30:31 +0100

    Revert "enable as cassandra entity"
    This reverts commit 46f85b37b89d934a5d49884f857ab1e6a8dca651.

--- grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/CommonTypes.groovy
+++ grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/CommonTypes.groovy
@@ -1,10 +1,25 @@
 package grails.gorm.tests;
 
-import grails.gorm.CassandraEntity
+import java.io.Serializable;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Currency;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.UUID;
 
-@CassandraEntity
+import grails.persistence.Entity
+
+@Entity
 class CommonTypes implements Serializable {
-    
+    UUID id
+    Long version
+    Long l
+    Byte b
+    Short s
+    Boolean bool
+    Integer i
     URL url
     Date date
     Calendar c
