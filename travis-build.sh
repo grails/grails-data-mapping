feb44cea6804e7e1cba0c7e18e8c22ee114b3e97
Graeme Rocher <graeme.rocher@gmail.com>
2014-06-10 10:59:51 +0200

    add Redis to Travis CI

--- travis-build.sh
+++ travis-build.sh
@@ -5,6 +5,7 @@ EXIT_STATUS=0
 ./gradlew grails-datastore-gorm-hibernate:test || EXIT_STATUS=$?
 ./gradlew grails-datastore-gorm-hibernate4:test || EXIT_STATUS=$?
 ./gradlew grails-datastore-gorm-mongodb:test || EXIT_STATUS=$?
+./gradlew grails-datastore-gorm-redis:test || EXIT_STATUS=$?
 ./gradlew grails-datastore-gorm-test:test || EXIT_STATUS=$?
 exit $EXIT_STATUS
 
