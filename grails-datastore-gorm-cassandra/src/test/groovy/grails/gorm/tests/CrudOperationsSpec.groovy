591a4f3afc52fa2ee3b5fa42ba51f3cd81523d02
Paras Lakhani <paras@atoms.to>
2014-06-11 21:47:52 +0100

    add tests for entity with assigned id

--- grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/CrudOperationsSpec.groovy
+++ grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/CrudOperationsSpec.groovy
@@ -11,13 +11,11 @@ class CrudOperationsSpec extends GormDatastoreSpec {
 	
     void "Test get using a string-based key"() {
         given:
-
             def t = new TestEntity(name:"Bob")
             t.save(flush:true)
             session.clear()
         when:
             t = TestEntity.get("${t.id}")
-
         then:
             t != null
     }
@@ -25,25 +23,33 @@ class CrudOperationsSpec extends GormDatastoreSpec {
     void "Test get returns null of non-existent entity"() {
         given:
             def t
+			def p
         when:
             t = TestEntity.get("1")
+			p = PersonAssignedId.get("none")
         then:
             t == null
+			p == null
         when: 
             t = TestEntity.get(UUIDUtil.randomTimeUUID)
+			p = PersonAssignedId.get([firstName:"none"])
         then:
             t == null
+			p == null
     }
 
     void "Test basic CRUD operations"() {
         given:
 
             def t = new TestEntity(name:"Bob")
+			def p = new PersonAssignedId(firstName: "Bob", lastName: "Wilson", age: 25)
             t.save()
-
+			p.save()
         when:
             def results = TestEntity.list()
             t = TestEntity.get(t.id)
+			def results2 = PersonAssignedId.list()
+			p = PersonAssignedId.get([firstName:p.firstName, lastName: p.lastName])
 
         then:
             t != null
@@ -51,6 +57,13 @@ class CrudOperationsSpec extends GormDatastoreSpec {
             "Bob" == t.name
             1 == results.size()
             "Bob" == results[0].name
+			
+			p != null
+			"Bob" == p.firstName
+			"Wilson" == p.lastName
+			25 == p.age
+			1 == results2.size()
+			"Bob" == results2[0].firstName
     }
 
     void "Test save method that takes a map"() {
