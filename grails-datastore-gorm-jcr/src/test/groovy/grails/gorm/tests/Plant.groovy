b21eb87f22fa2f7fcb4444a3f90988732269d7f5
Graeme Rocher <graemerocher@Graemes-MacBook-Air-2.local>
2014-06-13 00:57:23 +0200

    Updating docs for Travis build: https://travis-ci.org/grails/grails-data-mapping/builds/

--- grails-datastore-gorm-jcr/src/test/groovy/grails/gorm/tests/Plant.groovy
+++
@@ -1,9 +0,0 @@
-@grails.persistence.Entity
-class Plant implements Serializable{
-    String id
-    Long version
-    boolean goesInPatch
-    String name
-
-
-}
--- grails-datastore-gorm-jpa/src/test/groovy/grails/gorm/tests/City.groovy
+++
@@ -1,9 +0,0 @@
-package grails.gorm.tests
-
-import grails.gorm.JpaEntity
-
-@JpaEntity
-class City extends Location {
-    BigDecimal latitude
-    BigDecimal longitude
-}
