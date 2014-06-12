1ed68385f2eafc741aab3c99aff0ff239f68b968
Graeme Rocher <graemerocher@Graemes-MacBook-Air-2.local>
2014-06-13 00:59:39 +0200

    reinstate test running

--- travis-build.sh
+++ travis-build.sh
@@ -2,11 +2,11 @@ export GRADLE_OPTS="-Xmx2048m -Xms256m -XX:MaxPermSize=768m -XX:+CMSClassUnloadi
 
 
 EXIT_STATUS=0
-#./gradlew grails-datastore-gorm-hibernate:test || EXIT_STATUS=$?
-#./gradlew grails-datastore-gorm-hibernate4:test || EXIT_STATUS=$?
-#./gradlew grails-datastore-gorm-mongodb:test || EXIT_STATUS=$?
-#./gradlew grails-datastore-gorm-redis:test || EXIT_STATUS=$?
-#./gradlew grails-datastore-gorm-test:test || EXIT_STATUS=$?
+./gradlew grails-datastore-gorm-hibernate:test || EXIT_STATUS=$?
+./gradlew grails-datastore-gorm-hibernate4:test || EXIT_STATUS=$?
+./gradlew grails-datastore-gorm-mongodb:test || EXIT_STATUS=$?
+./gradlew grails-datastore-gorm-redis:test || EXIT_STATUS=$?
+./gradlew grails-datastore-gorm-test:test || EXIT_STATUS=$?
 
 
 version=$(grep 'projectVersion =' build.gradle)
