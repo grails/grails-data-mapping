eb53bcbdb8ea591f8f648cac829d54df7220f5e9
Paras Lakhani <paras@atoms.to>
2014-07-10 20:09:21 +0100

    changed to make work with Cassandra, offset unsupported

--- grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/PagedResultSpec.groovy
+++ grails-datastore-gorm-cassandra/src/test/groovy/grails/gorm/tests/PagedResultSpec.groovy
@@ -7,14 +7,20 @@ class PagedResultSpec extends GormDatastoreSpec {
             createPeople()
 
         when:"The list method is used with pagination params"
-            def results = Person.list(offset:2, max:2)
+            def results = PersonLastNamePartitionKey.list(max:2)
 
         then:"You get a paged result list back"
             results.getClass().simpleName == 'PagedResultList' // Grails/Hibernate has a custom class in different package
             results.size() == 2
-            results[0].firstName == "Bart"
-            results[1].firstName == "Lisa"
+            results[0].firstName == "Barney"
+            results[1].firstName == "Fred"
             results.totalCount == 6
+       
+        when:  
+            PersonLastNamePartitionKey.list(offset:1, max:2).size()
+        then: //offset not supported by Cassandra
+            thrown UnsupportedOperationException
+            
     }
 
     void "Test that a paged result list is returned from the critera with pagination params"() {
@@ -22,24 +28,24 @@ class PagedResultSpec extends GormDatastoreSpec {
             createPeople()
 
         when:"The list method is used with pagination params"
-            def results = Person.createCriteria().list(offset:1, max:2) {
+            def results = PersonLastNamePartitionKey.createCriteria().list(max:2) {
                 eq 'lastName', 'Simpson'
             }
 
         then:"You get a paged result list back"
             results.getClass().simpleName == 'PagedResultList' // Grails/Hibernate has a custom class in different package
             results.size() == 2
-            results[0].firstName == "Marge"
-            results[1].firstName == "Bart"
+            results[0].firstName == "Bart"
+            results[1].firstName == "Homer"
             results.totalCount == 4
     }
 
     protected void createPeople() {
-        new Person(firstName: "Homer", lastName: "Simpson", age:45).save()
-        new Person(firstName: "Marge", lastName: "Simpson", age:40).save()
-        new Person(firstName: "Bart", lastName: "Simpson", age:9).save()
-        new Person(firstName: "Lisa", lastName: "Simpson", age:7).save()
-        new Person(firstName: "Barney", lastName: "Rubble", age:35).save()
-        new Person(firstName: "Fred", lastName: "Flinstone", age:41).save()
+        new PersonLastNamePartitionKey(firstName: "Homer", lastName: "Simpson", age:45).save()
+        new PersonLastNamePartitionKey(firstName: "Marge", lastName: "Simpson", age:40).save()
+        new PersonLastNamePartitionKey(firstName: "Bart", lastName: "Simpson", age:9).save()
+        new PersonLastNamePartitionKey(firstName: "Lisa", lastName: "Simpson", age:7).save()
+        new PersonLastNamePartitionKey(firstName: "Barney", lastName: "Rubble", age:35).save()
+        new PersonLastNamePartitionKey(firstName: "Fred", lastName: "Flinstone", age:41).save()
     }
 }
