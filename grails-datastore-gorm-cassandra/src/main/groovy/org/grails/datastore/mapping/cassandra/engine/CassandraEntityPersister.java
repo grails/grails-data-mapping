e945b25510007e80a88ecf16fa51232097f45336
Paras Lakhani <paras@atoms.to>
2014-07-23 15:35:05 +0100

    Handle Optimistic Locking on update Override setObjectIdentifier and CassandraEntityAccess.setIdentifier to handle compound primary keys. CassandraEntityAccess.setProperty now converts without converstion all properties except enum and version properties. The rest are being set on same object type anyway so don't require convertion

--- grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/engine/CassandraEntityPersister.java
+++ grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/engine/CassandraEntityPersister.java
@@ -29,6 +29,7 @@ import org.grails.datastore.mapping.cassandra.config.Column;
 import org.grails.datastore.mapping.cassandra.config.Table;
 import org.grails.datastore.mapping.cassandra.query.CassandraQuery;
 import org.grails.datastore.mapping.cassandra.uuid.UUIDUtil;
+import org.grails.datastore.mapping.core.OptimisticLockingException;
 import org.grails.datastore.mapping.engine.AssociationIndexer;
 import org.grails.datastore.mapping.engine.EntityAccess;
 import org.grails.datastore.mapping.engine.NativeEntryEntityPersister;
@@ -37,15 +38,24 @@ import org.grails.datastore.mapping.model.ClassMapping;
 import org.grails.datastore.mapping.model.MappingContext;
 import org.grails.datastore.mapping.model.PersistentEntity;
 import org.grails.datastore.mapping.model.PersistentProperty;
+import org.grails.datastore.mapping.model.config.GormProperties;
 import org.grails.datastore.mapping.model.types.Association;
 import org.grails.datastore.mapping.proxy.ProxyFactory;
 import org.grails.datastore.mapping.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cassandra.core.SessionCallback;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.cassandra.core.CassandraTemplate;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Update;
 
 /**
  * @author Graeme Rocher
@@ -54,7 +64,7 @@ import org.springframework.data.cassandra.core.CassandraTemplate;
 @SuppressWarnings("rawtypes")
 public class CassandraEntityPersister extends NativeEntryEntityPersister<EntityAccess, Object> {
 
-    private static Logger log = LoggerFactory.getLogger(CassandraEntityPersister.class);
+    private static Logger LOG = LoggerFactory.getLogger(CassandraEntityPersister.class);
     private org.springframework.data.cassandra.mapping.CassandraPersistentEntity<?> springCassandraPersistentEntity;
     private CassandraTemplate cassandraTemplate;
     private ConversionService conversionService;
@@ -172,8 +182,52 @@ public class CassandraEntityPersister extends NativeEntryEntityPersister<EntityA
     }
 
     @Override
-    protected void updateEntry(PersistentEntity persistentEntity, EntityAccess entityAccess, Object key, EntityAccess entry) {
-        cassandraTemplate.update(entityAccess.getEntity());
+    protected void updateEntry(final PersistentEntity persistentEntity, final EntityAccess entityAccess, final Object key, final EntityAccess entry) {       
+        Object entity = entityAccess.getEntity();          
+        if (isVersioned(entityAccess)) {
+            Object currentVersion = getCurrentVersion(entityAccess);
+            //TODO: bug in Spring Cassandra throwing NPE in exception translation. Throw OptimisticLockingException directly in SessionCallback below once fixed.
+            final Boolean[] optimisticLockException = {false};        
+            incrementVersion(entityAccess);
+            if (currentVersion != null) {
+                final Update update = CassandraTemplate.toUpdateQuery(cassandraTemplate.getTableName(entity.getClass()).toCql(), entity, null, cassandraTemplate.getConverter());
+                update.onlyIf(QueryBuilder.eq(GormProperties.VERSION, currentVersion));
+                cassandraTemplate.execute(new SessionCallback<Object>() {
+                    @Override
+                    public Object doInSession(Session s) throws DataAccessException {
+                        if (LOG.isDebugEnabled()) {
+                            LOG.debug("Cassandra Update: " + update.toString());
+                        }
+                        ResultSet resultSet = s.execute(update);
+                        Row row = resultSet.one();
+
+                        if (row.getBool("[applied]")) {
+                            if (LOG.isDebugEnabled()) {
+                                LOG.debug("Successfully modified entry " + key + " to version " + getCurrentVersion(entityAccess));
+                            }
+                        } else {
+                            optimisticLockException[0] = true;
+                            
+                        }
+                        return null;
+                    }
+                });
+            }
+            if (optimisticLockException[0]) {
+                throw new OptimisticLockingException(persistentEntity, key);
+            }
+            return;
+        }
+        cassandraTemplate.update(entity);
+        
+    }
+    
+    protected Object getCurrentVersion(final EntityAccess ea) {
+        Object currentVersion = ea.getProperty(GormProperties.VERSION);
+        if (Number.class.isAssignableFrom(ea.getPropertyType(GormProperties.VERSION))) {
+            currentVersion = currentVersion != null ? ((Number)currentVersion).longValue() : currentVersion;
+        }
+        return currentVersion;
     }
 
     @SuppressWarnings("unchecked")
@@ -205,6 +259,11 @@ public class CassandraEntityPersister extends NativeEntryEntityPersister<EntityA
         return id;
     }
 
+    @Override
+    public void setObjectIdentifier(Object obj, Serializable id) {
+        new CassandraEntityAccess(getPersistentEntity(), obj).setIdentifier(id);
+    }
+    
     @Override
     public Query createQuery() {
         return new CassandraQuery(getCassandraSession(), getPersistentEntity());
@@ -243,14 +302,33 @@ public class CassandraEntityPersister extends NativeEntryEntityPersister<EntityA
         }
         
         @Override
-        public void setProperty(String name, Object value) {
-        	final Table table = (Table) classMapping.getMappedForm();
-        	if (table.isPrimaryKey(name) && value instanceof Map) {
-        		value = ((Map)value).get(name);
-        	}
-        	//TODO: change back to setPropertyNoConversion and handle version property separately?
-        	super.setProperty(name, value);
+        public void setIdentifier(Object id) {
+            final Table table = (Table) classMapping.getMappedForm();
+            if (table.hasCompositePrimaryKeys() && id instanceof Map) {
+                Map idMap = (Map) id;
+                for (String primaryKeyName : table.getPrimaryKeyNames()) {
+                    Object value = idMap.get(primaryKeyName);
+                    if (value != null) {
+                        setProperty(primaryKeyName, value);
+                    }
+                }
+            } else {
+                super.setIdentifier(id);
+            }
         }
-
+        
+        @Override
+        public void setProperty(String name, Object value) {
+            final Table table = (Table) classMapping.getMappedForm();
+            if (table.isPrimaryKey(name) && value instanceof Map) {
+                value = ((Map)value).get(name);
+            }
+            Class type = getPropertyType(name);
+            if(type.isEnum() || GormProperties.VERSION.equals(name)) {
+                super.setProperty(name, value);    
+            } else {
+                super.setPropertyNoConversion(name, value);
+            }            
+        }
     }
 }
