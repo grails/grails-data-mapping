cf3fcebb29cd8364097259c2268cf789373f0cf4
Paras Lakhani <paras@atoms.to>
2014-07-23 15:26:13 +0100

    Handle duplicate column created if id property in constraints block Handle default sort column

--- grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/config/GormCassandraMappingFactory.java
+++ grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/config/GormCassandraMappingFactory.java
@@ -1,6 +1,7 @@
 package org.grails.datastore.mapping.cassandra.config;
 
 import java.beans.PropertyDescriptor;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -13,6 +14,7 @@ import org.grails.datastore.mapping.model.PersistentEntity;
 import org.grails.datastore.mapping.model.PersistentProperty;
 import org.grails.datastore.mapping.model.PropertyMapping;
 import org.grails.datastore.mapping.model.types.Identity;
+import org.grails.datastore.mapping.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,10 +37,21 @@ public class GormCassandraMappingFactory extends AbstractGormMappingFactory<Tabl
         }
 
         Map<String, Column> properties = entityToPropertyMap.get(entity);
-        for (Entry<String, Column> entry : properties.entrySet()) {
+        Column idProperty  = properties.get(IDENTITY_PROPERTY);
+        Iterator<Entry<String, Column>> iterator = properties.entrySet().iterator();
+        
+        while (iterator.hasNext()) {
+            Entry<String, Column> entry = iterator.next();                  
             if (entry.getValue() instanceof Column) {
                 String name = entry.getKey();
                 Column column = entry.getValue();
+                if (idProperty != null && idProperty.getName() != null && idProperty.getName().equals(name)) {
+                    //remove extra column created if id property in constraints block, will be handled elsewhere,
+                    //as it conflicts with the column created in mapping block.                    
+                    iterator.remove();
+                    continue;
+                }
+                
                 if (column.getName() == null) {
                     column.setName(name);
                 }
@@ -47,7 +60,18 @@ public class GormCassandraMappingFactory extends AbstractGormMappingFactory<Tabl
                 }
             }
         }
-
+        
+        Map<String, String> sort = table.getSort();
+        if (sort != null && sort.size() > 0) {
+            Entry<String, String> entry = sort.entrySet().iterator().next();
+            String property = entry.getKey();
+            if (properties.containsKey(property)) {
+                String direction = entry.getValue();
+                Query.Order orderBy = "desc".equals(direction) ? Query.Order.desc(property) : Query.Order.asc(property);
+                table.setOrderBy(orderBy);                
+            }
+        }
+        
         return table;
     }
 
