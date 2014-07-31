b1ef98f196482845656eba76e154b8e3d6663e86
Paras Lakhani <paras@atoms.to>
2014-07-31 16:50:18 +0100

    Changed sort handling so its parsed here and not GormCassandraMappingFactory, extra property no longer needed

--- grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/config/Table.java
+++ grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/config/Table.java
@@ -5,17 +5,21 @@ import java.util.List;
 import java.util.Map;
 
 import org.grails.datastore.mapping.config.Entity;
-import org.grails.datastore.mapping.query.Query.Order;
+import org.grails.datastore.mapping.query.Query;
 
+/**
+ * Provides configuration options for mapping Cassandra tables
+ *
+ */
+@SuppressWarnings("rawtypes")
 public class Table extends Entity {
     private String keyspace;
     private List<Column> primaryKeys = new ArrayList<Column>();
     private List<String> primaryKeyNames = new ArrayList<String>();
-    private Map<String, String> sort;
-    private Order orderBy;
+    private Query.Order sort;          
     
     public Table() {
-     
+        
     }
     
     public Table(String keyspace ) {
@@ -49,21 +53,30 @@ public class Table extends Entity {
     public boolean isPrimaryKey(String name) {
         return primaryKeyNames.contains(name);
     }
-        
-    public void setSort(Map<String, String> sort) {
-        this.sort = sort;
-    }
-    
-    public Map<String, String> getSort() {
+
+    public Query.Order getSort() {
         return sort;
     }
-
-    public Order getOrderBy() {
-        return orderBy;
-    }
-
-    public void setOrderBy(Order orderBy) {
-        this.orderBy = orderBy;
-    }   
     
+    public void setSort(Object sort) {
+        if (sort instanceof Query.Order) {
+            this.sort = (Query.Order) sort;
+        }
+        if (sort instanceof Map) {
+            Map m = (Map) sort;
+            if (!m.isEmpty()) {
+                Map.Entry entry = (Map.Entry) m.entrySet().iterator().next();
+                Object key = entry.getKey();
+                if ("desc".equalsIgnoreCase(entry.getValue().toString())) {
+                    this.sort = Query.Order.desc(key.toString());
+                }
+                else {
+                    this.sort = Query.Order.asc(key.toString());
+                }
+            }
+        }
+        else {
+            this.sort = Query.Order.asc(sort.toString());
+        }
+    }               
 }
