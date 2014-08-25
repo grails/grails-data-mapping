a151e5f8d866e60bf89aa61bf4023cf7a0c5dbea
Lari Hotari <lhotari@pivotal.io>
2014-08-25 20:31:35 +0300

    attempt to fix Travis build failure

--- travis-build.sh
+++ travis-build.sh
@@ -1,5 +1,4 @@
-export GRADLE_OPTS="-Xmx2048m -Xms256m -XX:MaxPermSize=768m -XX:+CMSClassUnloadingEnabled -XX:+HeapDumpOnOutOfMemoryError" 
-
+#!/bin/bash
 
 EXIT_STATUS=0
 ./gradlew grails-datastore-gorm-hibernate:test || EXIT_STATUS=$?
@@ -7,7 +6,6 @@ EXIT_STATUS=0
 ./gradlew grails-datastore-gorm-mongodb:test || EXIT_STATUS=$?
 ./gradlew grails-datastore-gorm-redis:test || EXIT_STATUS=$?
 ./gradlew grails-datastore-gorm-test:test || EXIT_STATUS=$?
-
 
 version=$(grep 'projectVersion =' build.gradle)
 version=${version//[[:blank:]]/}
