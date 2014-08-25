e97dee3d572fab718cd21b6d5dab3c93dfb4c1e8
Paras Lakhani <paras@atoms.to>
2014-08-25 23:44:51 +0100

    Default mapping closure handling

--- grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/config/CassandraMappingContext.java
+++ grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/config/CassandraMappingContext.java
@@ -1,5 +1,7 @@
 package org.grails.datastore.mapping.cassandra.config;
 
+import groovy.lang.Closure;
+
 import org.grails.datastore.gorm.cassandra.mapping.BasicCassandraMappingContext;
 import org.grails.datastore.mapping.model.AbstractMappingContext;
 import org.grails.datastore.mapping.model.MappingConfigurationStrategy;
@@ -12,32 +14,46 @@ import org.springframework.util.Assert;
  * Started by Jeff Beck(@beckje01) on 1/23/14.
  * 
  */
+@SuppressWarnings({"rawtypes"})
 public class CassandraMappingContext extends AbstractMappingContext {
     public static final String DEFAULT_KEYSPACE = "CassandraKeySpace";
     protected String keyspace;
     protected MappingFactory<Table, Column> mappingFactory;
     protected MappingConfigurationStrategy syntaxStrategy;
     protected BasicCassandraMappingContext springCassandraMappingContext;     
+    private Closure defaultMapping;
     
     public CassandraMappingContext() {
-        this(DEFAULT_KEYSPACE);
+        this(DEFAULT_KEYSPACE, null);
     }
+    
+    public CassandraMappingContext(String keyspace) {
+    	this(keyspace, null);
+    }
+    
     /**
      * Constructs a context using the given keyspace
      *
      * @param keyspace
      *            The keyspace, this is typically the application name
      */
-    public CassandraMappingContext(String keyspace) {
-        Assert.hasText(keyspace, "Property [keyspace] must be set");
+    public CassandraMappingContext(String keyspace, Closure defaultMapping) {
+        Assert.hasText(keyspace, "Property [keyspace] must be set!");
         this.keyspace = keyspace;
+        this.defaultMapping = defaultMapping;
         mappingFactory = createMappingFactory();
         syntaxStrategy = new GormMappingConfigurationStrategy(mappingFactory);
 
     }
 
+    public Closure getDefaultMapping() {
+        return defaultMapping;
+    }
+    
     protected MappingFactory<Table, Column> createMappingFactory() {
-        return new GormCassandraMappingFactory(keyspace);
+        GormCassandraMappingFactory cassandraMappingFactory = new GormCassandraMappingFactory(keyspace);
+        cassandraMappingFactory.setDefaultMapping(defaultMapping);
+        return cassandraMappingFactory;
     }
 
     public String getKeyspace() {
