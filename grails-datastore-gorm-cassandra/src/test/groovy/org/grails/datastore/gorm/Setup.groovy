eb9762c770da106dff06bda6bc6cebb74042e811
Paras Lakhani <paras@atoms.to>
2014-08-25 23:52:26 +0100

    Allow subclass defined entities to be added to mapping context and have schema created. Add IF NOT EXISTS to keyspace creation so don't have to check if it exists first

--- grails-datastore-gorm-cassandra/src/test/groovy/org/grails/datastore/gorm/Setup.groovy
+++ grails-datastore-gorm-cassandra/src/test/groovy/org/grails/datastore/gorm/Setup.groovy
@@ -5,21 +5,23 @@ import org.grails.datastore.gorm.events.AutoTimestampEventListener
 import org.grails.datastore.gorm.events.DomainEventListener
 import org.grails.datastore.mapping.cassandra.CassandraDatastore
 import org.grails.datastore.mapping.cassandra.config.CassandraMappingContext
+import org.grails.datastore.mapping.cassandra.config.CassandraPersistentEntity
 import org.grails.datastore.mapping.core.Session
 import org.grails.datastore.mapping.model.PersistentEntity
 import org.grails.datastore.mapping.transactions.DatastoreTransactionManager
 import org.springframework.context.support.GenericApplicationContext
-import org.springframework.data.cassandra.mapping.CassandraPersistentEntity
+import org.springframework.data.cassandra.core.CassandraAdminTemplate
+import org.springframework.data.cassandra.core.CassandraTemplate
 import org.springframework.util.StringUtils
 import org.springframework.validation.Errors
 import org.springframework.validation.Validator
-
-import com.datastax.driver.core.KeyspaceMetadata
 
 
 class Setup {
 
     static CassandraDatastore cassandraDatastore
+	static CassandraTemplate cassandraTemplate
+	static CassandraAdminTemplate cassandraAdminTemplate
     static String keyspace = "unittest"
 
     static destroy() {
@@ -36,7 +38,9 @@ class Setup {
         return caught
     }
 
-    static Session setup(List<Class> classes) {        
+    static Session setup(List<Class> classes, List<Class> additionalClasses) {        
+		// need to split out cluster creation, keyspace creation, persistent entity addition and session creation 
+		// in order to create nativeSession and schema's in the order defined by Spring Data Cassandra
         if (cassandraDatastore == null) {
             def ctx = new GenericApplicationContext()
             ctx.refresh()
@@ -44,10 +48,13 @@ class Setup {
             ConfigObject config = new ConfigObject()            
             config.setProperty(CassandraDatastore.CASSANDRA_SCHEMA_ACTION, "RECREATE_DROP_UNUSED")
             //config.setProperty(CassandraDatastore.CASSANDRA_CONTACT_POINTS, "localhost")
-            cassandraDatastore = new CassandraDatastore(new CassandraMappingContext("unittest"), config, ctx)
-            cassandraDatastore.createCluster()
+            def cassandraDatastore = new CassandraDatastore(new CassandraMappingContext("unittest"), config, ctx)
+            def nativeCluster = cassandraDatastore.createCluster()
+			if (!nativeCluster) {
+				throw new RuntimeException("Cassandra Template not found, possible reason: Spring Data Cassandra not initialized")
+			}
 
-            ensureKeyspace()
+            ensureKeyspace(nativeCluster)
 
             def entities = []
             for (cls in classes) {
@@ -69,46 +76,49 @@ class Setup {
             cassandraDatastore.applicationContext.addApplicationListener new AutoTimestampEventListener(cassandraDatastore)
             
             cassandraDatastore.afterPropertiesSet()
+			
+			this.cassandraDatastore = cassandraDatastore
+			cassandraTemplate = cassandraDatastore.cassandraTemplate
+			if (!cassandraTemplate) {
+				throw new RuntimeException("Cassandra Template not found, possible reason: Spring Data Cassandra not initialized")
+			}
+			cassandraAdminTemplate = new CassandraAdminTemplate(cassandraDatastore.nativeSession, cassandraDatastore.cassandraTemplate.cassandraConverter)
         }
-        
+		
+		for (cls in additionalClasses) {
+			if (!cassandraDatastore.mappingContext.isPersistentEntity(cls)) {
+				cassandraDatastore.mappingContext.addPersistentEntity(cls)
+				cassandraDatastore.createTableDefinition(cls)				
+			}
+		}
+		
         def txMgr = new DatastoreTransactionManager(datastore: cassandraDatastore)
         CassandraMethodsConfigurer methodsConfigurer = new CassandraMethodsConfigurer(cassandraDatastore, txMgr)
         methodsConfigurer.configure()
         
-        def cassandraSession = cassandraDatastore.connect()
-        
-        deleteAllEntities(cassandraSession)
-
+        def cassandraSession = cassandraDatastore.connect()     
+        truncateAllEntities()
+               
         return cassandraSession
     }
 
-    private static void ensureKeyspace() {
-        def cluster = cassandraDatastore.getNativeCluster()
-
+    private static void ensureKeyspace(nativeCluster) {        
         if (!StringUtils.hasText(keyspace)) {
             keyspace = null
         }
 
-        if (keyspace != null) {
-            // see if we need to create the keyspace
-            KeyspaceMetadata kmd = cluster.getMetadata().getKeyspace(keyspace)
-            if (kmd == null) { // then create keyspace
-
-                def cql = "CREATE KEYSPACE " + keyspace + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};"
-                println "creating keyspace ${keyspace} via CQL [${cql}]"
-                def session = cluster.connect()
-                session.execute(cql)
-                session.close()
-            }
-        }
+        if (keyspace != null) {           
+            def cql = "CREATE KEYSPACE IF NOT EXISTS " + keyspace + " WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};"
+            println "creating keyspace ${keyspace} via CQL [${cql}]"
+            def session = nativeCluster.connect()
+            session.execute(cql)
+            session.close()
+        }        
     }
 
-    public static void deleteAllEntities(def cassandraSession) {
-        if (!cassandraSession.cassandraTemplate) {
-            throw new RuntimeException("Cassandra Template not found, possible reason: Spring Data Cassandra not initialized")
-        }
-        for (CassandraPersistentEntity<?> entity : cassandraSession.cassandraTemplate?.cassandraConverter?.mappingContext?.persistentEntities) {
-            cassandraSession.cassandraTemplate.truncate(entity.getTableName())
+    public static void truncateAllEntities() {        
+        for (CassandraPersistentEntity entity : cassandraDatastore.mappingContext.persistentEntities) {
+            cassandraTemplate.truncate(cassandraTemplate.getTableName(entity.getJavaClass()))
         }
     }
 
