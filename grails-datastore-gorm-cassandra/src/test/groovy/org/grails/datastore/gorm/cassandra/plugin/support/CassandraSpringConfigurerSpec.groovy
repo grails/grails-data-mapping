45716f7c41dca3a4f6a518a60d633bdc089a67e8
Paras Lakhani <paras@atoms.to>
2014-09-05 23:17:36 +0100

    Added keyspace creation tests

--- grails-datastore-gorm-cassandra/src/test/groovy/org/grails/datastore/gorm/cassandra/plugin/support/CassandraSpringConfigurerSpec.groovy
+++ grails-datastore-gorm-cassandra/src/test/groovy/org/grails/datastore/gorm/cassandra/plugin/support/CassandraSpringConfigurerSpec.groovy
@@ -7,54 +7,61 @@ import org.codehaus.groovy.grails.commons.DefaultGrailsApplication
 import org.codehaus.groovy.grails.commons.DomainClassArtefactHandler
 import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.codehaus.groovy.grails.plugins.DefaultGrailsPluginManager
-import org.grails.datastore.gorm.Setup
+import org.grails.datastore.mapping.cassandra.CassandraDatastore
 import org.grails.datastore.mapping.cassandra.config.CassandraMappingContext
 import org.springframework.data.cassandra.core.CassandraTemplate
 
 import spock.lang.Specification
+
+import com.datastax.driver.core.KeyspaceMetadata
 
 /**
  */
 class CassandraSpringConfigurerSpec extends Specification{
 
     void "Test configure Cassandra via Spring for config"() {
-        when:"The spring configurer is used"
-            def configurer = new CassandraSpringConfigurer()
-            def config = configurer.getConfiguration()
-            def application = new DefaultGrailsApplication([Person] as Class[], Thread.currentThread().contextClassLoader)
-
-
-            def keyspace = "randomKeyspaceTest"
-			def contactPoints = "10.0.0.1"
-            application.config.grails.cassandra.keyspace = keyspace
-			application.config.grails.cassandra.contactPoints = contactPoints
+		given: 
+    		def configurer = new CassandraSpringConfigurer()
+    		def config = configurer.getConfiguration()
+    		def application = new DefaultGrailsApplication([Person] as Class[], Thread.currentThread().contextClassLoader)
+    		def keyspace = randomKeyspaceName()
+    		def contactPoints = "10.0.0.1"
+    		application.config.grails.cassandra.keyspace.name = keyspace
+    		application.config.grails.cassandra.contactPoints = contactPoints
 			
+        when:"The spring configurer is used"		            	
             BeanBuilder bb = createInitialApplicationContext(application)
             bb.beans config
 
         then:"Then the cassandra beans have their config set"
             bb.getBeanDefinition('cassandraMappingContext').getPropertyValues().getPropertyValue('keyspace').value == keyspace
 			def i = bb.getBeanDefinition('cassandraDatastore').getPropertyValues().getPropertyValue('config').value.get("contactPoints") == contactPoints		
-            bb.getBeanDefinition('cassandraTemplate') != null
+            bb.getBeanDefinition('cassandraTemplate') != null											
     }
 
-    void "Test configure Cassandra via Spring"() {
-        when:"The spring configurer is used"
-            def configurer = new CassandraSpringConfigurer()
-            def config = configurer.getConfiguration()
-            final defaultConfig = {
-                version false
-            }
-            def application = new DefaultGrailsApplication([Person] as Class[], Thread.currentThread().contextClassLoader)
-			application.metadata.applicationName = Setup.keyspace
+    void "Test configure Cassandra keyspace via Spring"() {
+		given:
+    		def configurer = new CassandraSpringConfigurer()
+    		def config = configurer.getConfiguration()
+    		def keyspace = randomKeyspaceName()
+    		final defaultConfig = {
+    			version false
+    		}
+    		def application = new DefaultGrailsApplication([Person] as Class[], Thread.currentThread().contextClassLoader)
+    		application.config.grails.cassandra.keyspace.name = keyspace
+    		application.config.grails.cassandra.keyspace.action = "CREATE_DROP"
+			application.config.grails.cassandra.keyspace.durableWrites = false			
+			application.config.grails.cassandra.keyspace.replicationStrategy = "NetworkTopologyStrategy"
+			application.config.grails.cassandra.keyspace.networkTopology = ["us-east":1, "eu-west":2]
+			
+        when:"The spring configurer is used"		           
             BeanBuilder bb = createInitialApplicationContext(application, defaultConfig)
             bb.beans config
             def ctx = bb.createApplicationContext()
             CassandraMappingContext mappingContext = ctx.getBean("cassandraMappingContext",CassandraMappingContext)
             def entity = mappingContext?.getPersistentEntity(Person.name)
-			
-			
-        then:"The application context is created"
+				
+        then:"The application context cassandra beans and keyspace is created"
             ctx != null
             ctx.containsBean("persistenceInterceptor")
 			mappingContext.defaultMapping == defaultConfig
@@ -62,10 +69,27 @@ class CassandraSpringConfigurerSpec extends Specification{
 			def cassandraTemplate = ctx.getBean("cassandraTemplate", CassandraTemplate)
 			cassandraTemplate != null
 			cassandraTemplate.session != null
-			def cluster = cassandraTemplate.session.cluster
+			def cassandraDatastore = ctx.getBean("cassandraDatastore")
+			def cluster = cassandraDatastore.nativeCluster
 			cluster != null			
-            cluster.metadata.getKeyspace(Setup.keyspace) != null
-
+            KeyspaceMetadata keyspaceMeta = cluster.metadata.getKeyspace(keyspace)
+			keyspaceMeta != null
+			keyspaceMeta.name == keyspace						
+			keyspaceMeta.durableWrites == false
+			keyspaceMeta.replication['class'] == "org.apache.cassandra.locator.NetworkTopologyStrategy"						
+			
+		when:"CassandraDatastore created with keyspace already created"
+			def cassandraDatastore2 = new CassandraDatastore(mappingContext, [:], ctx)
+			cassandraDatastore2.afterPropertiesSet()
+		
+		then:"CassandraDatastore does not create a keyspace"
+			cassandraDatastore2.keyspaceActionSpecificationFactoryBean == null
+			def cluster2 = cassandraDatastore2.nativeCluster
+			cluster2 != null
+			def keyspaceMeta2 = cluster2.metadata.getKeyspace(keyspace)
+			keyspaceMeta2 != null
+			keyspaceMeta2.name == keyspace
+			cassandraDatastore.destroy() //instance that contains the keyspace destroy specification
     }
 
     protected BeanBuilder createInitialApplicationContext(GrailsApplication application, Closure defaultMappingConfig = {}) {
@@ -87,4 +111,8 @@ class CassandraSpringConfigurerSpec extends Specification{
 
         return bb
     }
+	
+	def String randomKeyspaceName() {
+		return "ks" + UUID.randomUUID().toString().replace("-", "")
+	}
 }
