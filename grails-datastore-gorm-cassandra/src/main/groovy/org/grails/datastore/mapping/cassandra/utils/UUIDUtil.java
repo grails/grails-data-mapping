1092f16af474b44def75a0d8cff02a6027321255
Paras Lakhani <paras@atoms.to>
2014-09-23 16:37:04 +0100

    Moved to utils package

--- grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/utils/UUIDUtil.java
+++ grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/utils/UUIDUtil.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.grails.datastore.mapping.cassandra.uuid;
+package org.grails.datastore.mapping.cassandra.utils;
 
 import com.datastax.driver.core.utils.UUIDs;
 
