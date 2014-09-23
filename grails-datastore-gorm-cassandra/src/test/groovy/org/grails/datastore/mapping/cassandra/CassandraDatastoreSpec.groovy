74db5fc2658ddb1c1ebd15e4feb337fc7b7abf12
Paras Lakhani <paras@atoms.to>
2014-09-23 18:46:08 +0100

    Added invalid keyspace test so shifted mock creation around to enable the test to work

--- grails-datastore-gorm-cassandra/src/test/groovy/org/grails/datastore/mapping/cassandra/CassandraDatastoreSpec.groovy
+++ grails-datastore-gorm-cassandra/src/test/groovy/org/grails/datastore/mapping/cassandra/CassandraDatastoreSpec.groovy
@@ -2,11 +2,11 @@ package org.grails.datastore.mapping.cassandra
 
 import org.grails.datastore.gorm.cassandra.mapping.MappingCassandraConverter
 import org.grails.datastore.mapping.cassandra.config.CassandraMappingContext
+import org.grails.datastore.mapping.model.IllegalMappingException
 import org.springframework.cassandra.config.CassandraCqlClusterFactoryBean
 import org.springframework.cassandra.config.KeyspaceAction
 import org.springframework.cassandra.config.KeyspaceActionSpecificationFactoryBean
 import org.springframework.cassandra.core.keyspace.KeyspaceOption.ReplicationStrategy
-import org.springframework.context.support.GenericApplicationContext
 import org.springframework.data.cassandra.config.SchemaAction
 
 import spock.lang.Specification
@@ -24,22 +24,23 @@ class CassandraDatastoreSpec extends Specification {
 	ConfigObject keyspaceConfig = new ConfigObject()
 	
 	def	setup() { 						
-		def mockCluster = Mock(Cluster)
-		1 * clusterBean.getObject() >> mockCluster		
-		1 * sessionBean.getObject() >> Mock(Session)		
-		1 * clusterBean.afterPropertiesSet()		
-		1 * sessionBean.setCluster(mockCluster)		
-		1 * sessionBean.afterPropertiesSet()
+		config.put(CassandraDatastore.KEYSPACE_CONFIG, keyspaceConfig)
 	}
 
-	private createCassandraDatastore(ConfigObject config) {
-		def ctx = new GenericApplicationContext()
-		ctx.refresh()
+	private createCassandraDatastoreWithMocks(ConfigObject config) {					
 		def keyspace = config.get(CassandraDatastore.KEYSPACE_CONFIG)?.get(CassandraDatastore.KEYSPACE_NAME)
-		CassandraDatastore cassandraDatastore = new CassandraDatastore(new CassandraMappingContext(keyspace), config, ctx)
+		CassandraDatastore cassandraDatastore = new CassandraDatastore(new CassandraMappingContext(keyspace), config, null)
 		cassandraDatastore.setCassandraCqlClusterFactoryBean(clusterBean)
 		cassandraDatastore.setKeyspaceActionSpecificationFactoryBean(keyspaceBean)
 		cassandraDatastore.setCassandraSessionFactoryBean(sessionBean)
+		
+		def mockCluster = Mock(Cluster)
+		1 * clusterBean.getObject() >> mockCluster
+		1 * sessionBean.getObject() >> Mock(Session)
+		1 * clusterBean.afterPropertiesSet()
+		1 * sessionBean.setCluster(mockCluster)
+		1 * sessionBean.afterPropertiesSet()
+		
 		cassandraDatastore
 	}	
 	
@@ -49,7 +50,7 @@ class CassandraDatastoreSpec extends Specification {
 			config = new ConfigSlurper().parse('''
 				grails.cassandra.keyspace.name="default"
 			''')?.grails.cassandra			    	
-    		CassandraDatastore cassandraDatastore = createCassandraDatastore(config)
+    		CassandraDatastore cassandraDatastore = createCassandraDatastoreWithMocks(config)
 			
 		when:
 			cassandraDatastore.afterPropertiesSet()
@@ -71,7 +72,50 @@ class CassandraDatastoreSpec extends Specification {
     		cassandraDatastore.cassandraAdminTemplate != null
 	}
 	
-	void "Test create new keyspace, custom contact points and port, no durable writes" () {	
+	void "Test configure CassandraDatastore with invalid keyspace action"() {
+		given:
+			def keyspace = "default"
+			config = new ConfigSlurper().parse('''
+				grails.cassandra.keyspace.name="default"
+				grails.cassandra.keyspace.action="Invalid"
+			''')?.grails.cassandra			    	
+			CassandraDatastore cassandraDatastore = new CassandraDatastore(new CassandraMappingContext(keyspace), config, null)
+			
+		when:
+			cassandraDatastore.afterPropertiesSet()
+
+		then:
+			def e = thrown(IllegalMappingException)
+			e.message.startsWith("Invalid option")
+	}
+	
+	void "Test create keyspace with config objects"() {
+		given:
+			def keyspace = "newkeyspace"			
+			keyspaceConfig.put(CassandraDatastore.KEYSPACE_NAME, keyspace)
+			keyspaceConfig.put(CassandraDatastore.KEYSPACE_ACTION, "CREATE_DROP")
+			keyspaceConfig.put(CassandraDatastore.KEYSPACE_DURABLE_WRITES, "false")
+			keyspaceConfig.put(CassandraDatastore.KEYSPACE_REPLICATION_STRATEGY, "SimpleStrategy")
+			keyspaceConfig.put(CassandraDatastore.KEYSPACE_REPLICATION_FACTOR, 2)
+			CassandraDatastore cassandraDatastore = createCassandraDatastoreWithMocks(config)
+		
+		when:
+			cassandraDatastore.afterPropertiesSet()
+		
+		then:
+			1 * clusterBean.setKeyspaceSpecifications(_ as Set)
+		
+			1 * keyspaceBean.setName(keyspace)
+			1 * keyspaceBean.setAction(KeyspaceAction.CREATE_DROP)
+			1 * keyspaceBean.setReplicationStrategy(ReplicationStrategy.SIMPLE_STRATEGY)
+			1 * keyspaceBean.setReplicationFactor(2)
+			1 * keyspaceBean.setDurableWrites(false)
+			1 * keyspaceBean.setIfNotExists(true)
+			1 * keyspaceBean.afterPropertiesSet()
+			1 * keyspaceBean.getObject() >> new HashSet()
+	}
+	
+	void "Test create keyspace, custom contact points and port, no durable writes" () {	
 		given: 
 			def keyspace = "newkeyspace"	
 			config = new ConfigSlurper().parse('''
@@ -88,7 +132,7 @@ class CassandraDatastoreSpec extends Specification {
                 	}
                 }
 			''')?.grails.cassandra			    	
-			CassandraDatastore cassandraDatastore = createCassandraDatastore(config)
+			CassandraDatastore cassandraDatastore = createCassandraDatastoreWithMocks(config)
 			
 		when:
 			cassandraDatastore.afterPropertiesSet()
@@ -110,36 +154,9 @@ class CassandraDatastoreSpec extends Specification {
 			1 * sessionBean.setKeyspaceName(keyspace)	
 			1 * sessionBean.setSchemaAction(SchemaAction.RECREATE_DROP_UNUSED)
 			
-	}
+	}	
 	
-	void "Test custom keyspace"() {
-		given:
-    		def keyspace = "newkeyspace"			    	
-			config.put(CassandraDatastore.KEYSPACE_CONFIG, keyspaceConfig)
-			keyspaceConfig.put(CassandraDatastore.KEYSPACE_NAME, keyspace)
-    		keyspaceConfig.put(CassandraDatastore.KEYSPACE_ACTION, "CREATE_DROP")
-    		keyspaceConfig.put(CassandraDatastore.KEYSPACE_DURABLE_WRITES, "false")
-			keyspaceConfig.put(CassandraDatastore.KEYSPACE_REPLICATION_STRATEGY, "SimpleStrategy")
-			keyspaceConfig.put(CassandraDatastore.KEYSPACE_REPLICATION_FACTOR, 2)
-			CassandraDatastore cassandraDatastore = createCassandraDatastore(config)
-		
-		when:
-			cassandraDatastore.afterPropertiesSet()
-		
-		then:
-			1 * clusterBean.setKeyspaceSpecifications(_ as Set)
-		
-    		1 * keyspaceBean.setName(keyspace)
-    		1 * keyspaceBean.setAction(KeyspaceAction.CREATE_DROP)
-    		1 * keyspaceBean.setReplicationStrategy(ReplicationStrategy.SIMPLE_STRATEGY)
-    		1 * keyspaceBean.setReplicationFactor(2)
-    		1 * keyspaceBean.setDurableWrites(false)
-    		1 * keyspaceBean.setIfNotExists(true)
-    		1 * keyspaceBean.afterPropertiesSet()
-    		1 * keyspaceBean.getObject() >> new HashSet()
-	}
-	
-	void "Test network topology strategy"() {
+	void "Test create keyspace network topology strategy"() {
 		given:
 			def keyspace = "newkeyspace"
 			config = new ConfigSlurper().parse('''
@@ -159,7 +176,7 @@ class CassandraDatastoreSpec extends Specification {
                 }
 			''')?.grails.cassandra
     		    		
-			CassandraDatastore cassandraDatastore = createCassandraDatastore(config)
+			CassandraDatastore cassandraDatastore = createCassandraDatastoreWithMocks(config)
 		
 		when:
 			cassandraDatastore.afterPropertiesSet()
