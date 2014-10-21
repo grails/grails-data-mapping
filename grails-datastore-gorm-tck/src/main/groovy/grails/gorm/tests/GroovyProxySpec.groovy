71aeed812e2b2661a51ca9594c73aec8ed5c6bb8
Lari Hotari <lhotari@pivotal.io>
2014-10-21 14:19:18 +0300

    GRAILS-11614 related: run GroovyProxySpec with and without GroovyProxyFactory

--- grails-datastore-gorm-tck/src/main/groovy/grails/gorm/tests/GroovyProxySpec.groovy
+++ grails-datastore-gorm-tck/src/main/groovy/grails/gorm/tests/GroovyProxySpec.groovy
@@ -9,9 +9,11 @@ import org.springframework.dao.DataIntegrityViolationException
 class GroovyProxySpec extends GormDatastoreSpec {
 
     void "Test proxying of non-existent instance throws an exception"() {
-        given:"A groovy proxy factory"
-            session.mappingContext.proxyFactory = new GroovyProxyFactory()
-
+        setup:
+            if(useGroovyProxyFactory) {
+                session.mappingContext.proxyFactory = new GroovyProxyFactory()
+            }
+            
         when:"A proxy is loaded for an instance that doesn't exist"
             def location = Location.proxy(123)
 
@@ -27,12 +29,17 @@ class GroovyProxySpec extends GormDatastoreSpec {
 
         then:"An exception is thrown"
             thrown DataIntegrityViolationException
+            
+        where:
+            useGroovyProxyFactory << [true, false]
     }
 
     void "Test creation and behavior of Groovy proxies"() {
-
-        given:
-            session.mappingContext.proxyFactory = new GroovyProxyFactory()
+        setup:
+            if(useGroovyProxyFactory) {
+                session.mappingContext.proxyFactory = new GroovyProxyFactory()
+            }
+            
             def id = new Location(name:"United Kingdom", code:"UK").save(flush:true)?.id
             session.clear()
 
@@ -43,7 +50,7 @@ class GroovyProxySpec extends GormDatastoreSpec {
 
             location != null
             id == location.id
-            Location == location.class
+            Location.isInstance(location) == true
             null != location.metaClass
             false == location.isInitialized()
             false == location.initialized
@@ -53,34 +60,47 @@ class GroovyProxySpec extends GormDatastoreSpec {
             true == location.isInitialized()
             true == location.initialized
             null != location.target
-            Location == location.class
+            Location.isInstance(location) == true
             null != location.metaClass
+        where:
+            useGroovyProxyFactory << [true, false]
     }
 
     void "Test setting metaClass property on proxy"() {
-        given:
-            session.mappingContext.proxyFactory = new GroovyProxyFactory()
+        setup:
+            if(useGroovyProxyFactory) {
+                session.mappingContext.proxyFactory = new GroovyProxyFactory()
+            }
+
         when:
             def location = Location.proxy(123)
             location.metaClass = null
         then:
             location.metaClass != null
+        where:
+            useGroovyProxyFactory << [true, false]
     }
 
     void "Test calling setMetaClass method on proxy"() {
-        given:
-            session.mappingContext.proxyFactory = new GroovyProxyFactory()
+        setup:
+            if(useGroovyProxyFactory) {
+                session.mappingContext.proxyFactory = new GroovyProxyFactory()
+            }
+
         when:
             def location = Location.proxy(123)
             location.setMetaClass(null)
         then:
             location.metaClass != null
+        where:
+            useGroovyProxyFactory << [true, false]
     }
         
     void "Test creation and behavior of Groovy proxies with method call"() {
-
-        given:
-        session.mappingContext.proxyFactory = new GroovyProxyFactory()
+        setup:
+        if(useGroovyProxyFactory) {
+            session.mappingContext.proxyFactory = new GroovyProxyFactory()
+        }
         def id = new Location(name:"United Kingdom", code:"UK").save(flush:true)?.id
         session.clear()
 
@@ -91,7 +111,7 @@ class GroovyProxySpec extends GormDatastoreSpec {
 
         location != null
         id == location.id
-        Location == location.class
+        Location.isInstance(location) == true
         null != location.metaClass
         false == location.isInitialized()
         false == location.initialized
@@ -101,7 +121,9 @@ class GroovyProxySpec extends GormDatastoreSpec {
         true == location.isInitialized()
         true == location.initialized
         null != location.target
-        Location == location.class
+        Location.isInstance(location) == true
         null != location.metaClass
+        where:
+        useGroovyProxyFactory << [true, false]
     }
 }
