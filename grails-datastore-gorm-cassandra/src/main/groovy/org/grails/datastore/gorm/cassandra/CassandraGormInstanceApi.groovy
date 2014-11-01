d257e3f4dcb4d92d9e69032023dfa49cb26dccb6
Paras Lakhani <paras@atoms.to>
2014-11-01 19:03:58 +0000

    Added update, updateSimpleTypes and embedded cassandra collection manipulation methods

--- grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/gorm/cassandra/CassandraGormInstanceApi.groovy
+++ grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/gorm/cassandra/CassandraGormInstanceApi.groovy
@@ -5,6 +5,7 @@ import groovy.transform.CompileStatic
 import org.codehaus.groovy.runtime.InvokerHelper
 import org.grails.datastore.gorm.GormInstanceApi
 import org.grails.datastore.mapping.cassandra.CassandraDatastore
+import org.grails.datastore.mapping.cassandra.CassandraSession
 import org.grails.datastore.mapping.cassandra.engine.CassandraEntityPersister
 import org.grails.datastore.mapping.cassandra.utils.OptionsUtil
 import org.grails.datastore.mapping.core.Datastore
@@ -19,6 +20,10 @@ class CassandraGormInstanceApi<D> extends GormInstanceApi<D> {
         super(persistentClass, datastore)        
     } 
 	
+	private CassandraEntityPersister getCassandraEntityPersister(Session session) {
+		return (CassandraEntityPersister) session.getPersister(persistentClass)
+	}
+	
 	/**
 	 * Saves an object with the given parameters
 	 * @param instance The instance
@@ -32,7 +37,8 @@ class CassandraGormInstanceApi<D> extends GormInstanceApi<D> {
 		} as SessionCallback)
 	}
 	
-	protected D doSave(D instance, Map params, Session session, boolean isInsert = false) {
+	protected D doSave(D instance, Map params, Session s, boolean isInsert = false) {
+		CassandraSession session = (CassandraSession) s
 		boolean hasErrors = false
 		boolean validate = params?.containsKey("validate") ? params.validate : true
 		MetaMethod validateMethod = instance.respondsTo('validate', InvokerHelper.EMPTY_ARGS).find { MetaMethod mm -> mm.parameterTypes.length == 0 && !mm.vargsMethod}
@@ -52,9 +58,10 @@ class CassandraGormInstanceApi<D> extends GormInstanceApi<D> {
 			}
 			return null
 		}
-		if (params?.updateSimpleTypes) {			
-            CassandraEntityPersister persister = (CassandraEntityPersister) session.getPersister(persistentClass)            
-            persister.updateSimpleTypes(instance)			
+		if (params?.update) {			
+			session.update(instance)
+		} else if (params?.updateSingleTypes) {			                   
+            getCassandraEntityPersister(session).updateSingleTypes(instance)			
 		} else {
     		if (isInsert) {
     			session.insert(instance)
@@ -74,12 +81,93 @@ class CassandraGormInstanceApi<D> extends GormInstanceApi<D> {
 	}
 	
 	/**
-	 * Update an object's non collection, non map types only to the datastore
+	 * Update an instance without loading into the session
 	 * @param instance The instance
+	 * @param params The parameters
 	 * @return Returns the instance
 	 */
 	D update(D instance, Map params = [:]) {
-		params.updateSimpleTypes = true
+		params.update = true
 		save(instance, params)
+	}
+	
+	/**
+	 * Update an instance's non collection, non map types only without loading into the session
+	 * @param instance The instance
+	 * @param params The parameters
+	 * @return Returns the instance
+	 */
+	D updateSingleTypes(D instance, Map params = [:]) {
+		params.updateSingleTypes = true
+		save(instance, params)
+	}	
+	
+	/**
+	 * Add the specified element to the instance's embedded list, set or map and generate an update for the datastore
+	 * @param instance the instance containing the collection or map
+	 * @param propertyName the name of the embedded list, set or map
+	 * @param element the element to add
+	 * @param params The parameters
+	 */
+	def append(D instance, String propertyName, Object element, Map params = [:]) {
+		execute ({ Session session ->			
+			getCassandraEntityPersister(session).append(instance, propertyName, element, OptionsUtil.convertToWriteOptions(params))
+			if (params.flush) {
+				session.flush()
+			}
+		 } as SessionCallback)
+	}
+	
+	/**
+	 * Prepend the specified element to the instance's embedded list and generate an update for the datastore
+	 * @param instance the instance containing the list 
+	 * @param propertyName the name of the embedded list
+	 * @param element the element to prepend
+	 * @param params The parameters
+	 */
+	def prepend(D instance, String propertyName, Object element, Map params = [:]) {
+		execute ({ Session session ->
+			getCassandraEntityPersister(session).prepend(instance, propertyName, element, OptionsUtil.convertToWriteOptions(params))
+			if (params.flush) {
+				session.flush()
+			}
+		 } as SessionCallback)
+	}
+	
+	/**
+	 * Replace the specified element at the specified index in the instance's embedded list and generate an update for the datastore
+	 * @param instance the instance containing the list
+	 * @param propertyName the name of the embedded list
+	 * @param index the index of the element to replace
+	 * @param element the element to be stored at the specified index
+	 * @param params The parameters
+	 */
+	def replaceAt(D instance, String propertyName, int index, Object element, Map params = [:]) {
+		execute ({ Session session ->
+			getCassandraEntityPersister(session).replaceAt(instance, propertyName, index, element, OptionsUtil.convertToWriteOptions(params))
+			if (params.flush) {
+				session.flush()
+			}
+		 } as SessionCallback)
+	}
+	
+	/**
+	 * Remove the specified element, or the element at the specified index, from the instance's embedded list, set or map and generate an update for the datastore
+	 * @param instance the instance containing the collection or map
+	 * @param propertyName the name of the embedded list, set or map
+	 * @param item the element or index of the element to remove in the case of a list, the element in the case of a set or map
+	 * @param isIndex whether the specified item is an element or the index of the element to remove, only true if removing from a list using index, false otherwise 
+	 * @param params The parameters
+	 */
+	def deleteFrom(D instance, String propertyName, Object item, boolean isIndex, Map params = [:]) {
+		if (isIndex && !(item instanceof Integer || item instanceof int)) {
+			throw new IllegalArgumentException("Deleting item by index, item must be an integer")
+		}
+		execute ({ Session session ->
+			getCassandraEntityPersister(session).deleteFrom(instance, propertyName, item, isIndex, OptionsUtil.convertToWriteOptions(params))
+			if (params.flush) {
+				session.flush()
+			}
+		 } as SessionCallback)
 	}
 }
