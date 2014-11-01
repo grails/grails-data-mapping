e984ffa56eb08d8b25dfd1a12cc9938a33a9d765
Paras Lakhani <paras@atoms.to>
2014-11-01 20:40:32 +0000

    Changes made to better validate cassandra options

--- grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/utils/EnumUtil.java
+++ grails-datastore-gorm-cassandra/src/main/groovy/org/grails/datastore/mapping/cassandra/utils/EnumUtil.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.EnumUtils;
-import org.grails.datastore.mapping.model.IllegalMappingException;
 import org.springframework.cassandra.core.keyspace.Option;
 
 public class EnumUtil {
@@ -54,7 +53,7 @@ public class EnumUtil {
      * @param key the enum as a string as returned by its toString method 
      * @param map  the map to search in for the key
      * @return the enum, defaultValue if key has no value in the map
-     * @throws IllegalMappingException if the value in the map cannot be converted to the enum 
+     * @throws IllegalArgumentException if the value in the map cannot be converted to the enum 
      */
 	public static <E extends Enum<E>> E findEnum(Class<E> enumClass, String key, Map<String, Object> map, E defaultValue) {
 		if (map == null) {
@@ -69,10 +68,26 @@ public class EnumUtil {
 			throw new IllegalArgumentException(String.format("Invalid type for property [%s], expected java.lang.String", key));
 		}
 		
-		E enumValue = findEnum(enumClass, (String) value);
-		
+		E enumValue = getRequiredEnum(enumClass, key, (String) value);
+		return enumValue;
+	}
+
+	/**
+     * <p>Gets the enum for the specified class, throwing IllegalArgumentException if not found.</p>
+     *
+     * This method finds the enum based on its toString method or its getName method if its an instance of @link {@link Option}.
+     * 
+     * @param <E> the type of the enumeration
+     * @param enumClass  the class of the enum to query, not null
+     * @param property used in the exception message to inform caller which property is invalid
+     * @param value the enum as a string as returned by its toString method
+     * @return the enum
+     * @throws IllegalArgumentException if the enum is not found from the specified value, message indicates invalid property and the allowable values for it
+     */
+	public static <E extends Enum<E>> E getRequiredEnum(Class<E> enumClass, String property, String value) {
+		E enumValue = findEnum(enumClass, (String) value);		
 		if (enumValue == null) {			
-			throw new IllegalArgumentException(String.format("Invalid option [%s] for property [%s], allowable values are %s", value, key, getValidEnumList(enumClass)));
+			throw new IllegalArgumentException(String.format("Invalid option [%s] for property [%s], allowable values are %s", value, property, getValidEnumList(enumClass)));
 		}
 		return enumValue;
 	}
@@ -97,4 +112,28 @@ public class EnumUtil {
 		return allowable;
 	}
 
+	/**
+     * <p>Returns the enum mapped by the specified key in the specified map,
+     * returning the defaultValue if the key or map is null.
+     * </p>     
+     * @param <E> the type of the enumeration
+     * @param property  used in the exception message to inform caller which property is invalid
+     * @param key the key to look for in the specified map 
+     * @param map  the map to search in for the key
+     * @return the enum, defaultValue if the key or map is null
+     * @throws IllegalArgumentException if the key has no corresponding enum in the map
+     */
+	public static <E extends Enum<E>> E findMatchingEnum(String property, Object key, Map<String, E> map, E defaultValue) {
+		if (key == null || map == null) {
+			return defaultValue;
+		}
+		if (!(key instanceof String)) {
+			throw new IllegalArgumentException(String.format("Invalid type for property [%s], expected java.lang.String", key));
+		}
+		E enumValue = map.get(key);		
+		if (enumValue == null) {			
+			throw new IllegalArgumentException(String.format("Invalid option [%s] for property [%s], allowable values are %s", key, property, map.keySet()));
+		}
+		return enumValue;
+	}
 }
