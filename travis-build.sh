e61dcadd4e81e195caa57a86d7ded703f3d11fb1
Graeme Rocher <graemerocher@Graemes-iMac.local>
2015-02-11 14:46:40 +0100

    Revert "test publishing"
    This reverts commit e290d30ec81cc376a0a10bed1af0e5bfbb92defe.

--- travis-build.sh
+++ travis-build.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
+EXIT_STATUS=0
 
-./gradlew grails-datastore-core:bintrayUpload
+case "$GORM_IMPL"  in
+    hibernate)
+        ./gradlew grails-datastore-gorm-hibernate:test || EXIT_STATUS=$?
+        ;;
+    hibernate4)
+        ./gradlew grails-datastore-gorm-hibernate4:test || EXIT_STATUS=$?
+        ;;
+    mongodb)
+        ./gradlew grails-datastore-gorm-mongodb:test || EXIT_STATUS=$?
+        ;;
+    redis)
+        ./gradlew grails-datastore-gorm-redis:test || EXIT_STATUS=$?
+        ;;
+    cassandra)
+        # wait for Cassandra to start up
+        sleep 5               
+        ./gradlew grails-datastore-gorm-cassandra:test || EXIT_STATUS=$?
+        ;;
+    neo4j)
+        ./gradlew grails-datastore-gorm-neo4j:test || EXIT_STATUS=$?
+        ;;
+    restclient)
+        ./gradlew grails-datastore-gorm-rest-client:test || EXIT_STATUS=$?
+        ;;
+    *)
+        ./gradlew testClasses || EXIT_STATUS=$?
+        ./gradlew grails-datastore-gorm-test:test || EXIT_STATUS=$?
+        ;;
+esac
+
+exit $EXIT_STATUS
+
 
 
