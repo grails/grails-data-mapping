9d37d9d67f10ade089bcd3d86cfd6e6db6b0a320
Lari Hotari <lhotari@pivotal.io>
2015-02-23 09:38:57 -0500

    reformat / fix indent

--- travis-publish.sh
+++ travis-publish.sh
@@ -2,13 +2,13 @@
 
 # use travis_after_all.py for publishing only after all builds are successfull.
 if [[ "$BUILD_LEADER" == "YES" ]]; then
-    if [[ "$BUILD_AGGREGATE_STATUS" != "others_succeeded" ]]; then
-      echo "Some builds failed, not publishing."
-      exit 0
-    fi
-else
-    # not build leader, exit
+  if [[ "$BUILD_AGGREGATE_STATUS" != "others_succeeded" ]]; then
+    echo "Some builds failed, not publishing."
     exit 0
+  fi
+else
+  # not build leader, exit
+  exit 0
 fi
 
 echo "Publishing..."
@@ -17,60 +17,60 @@ EXIT_STATUS=0
 
 if [[ $TRAVIS_REPO_SLUG == "grails/grails-data-mapping" && $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]; then
 
-    echo "Publishing archives"
+  echo "Publishing archives"
 
-    if [[ -n $TRAVIS_TAG ]]; then
-        ./gradlew bintrayUpload || EXIT_STATUS=$?
-    else
-        ./gradlew publish || EXIT_STATUS=$?
+  if [[ -n $TRAVIS_TAG ]]; then
+    ./gradlew bintrayUpload || EXIT_STATUS=$?
+  else
+    ./gradlew publish || EXIT_STATUS=$?
+  fi
+
+  ./gradlew allDocs || EXIT_STATUS=$?
+
+  git config --global user.name "$GIT_NAME"
+  git config --global user.email "$GIT_EMAIL"
+  git config --global credential.helper "store --file=~/.git-credentials"
+  echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+  git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
+  cd gh-pages
+
+  # If this is the master branch then update the snapshot
+  if [[ $TRAVIS_BRANCH == 'master' ]]; then
+    mkdir -p snapshot
+    cp -r ../build/docs/. ./snapshot/
+
+    git add snapshot/*
+  fi
+
+  # If there is a tag present then this becomes the latest
+  if [[ -n $TRAVIS_TAG ]]; then
+    version="$TRAVIS_TAG"
+    version=${version:1}
+    milestone=${version:5}
+    if [[ -n $milestone ]]; then
+      mkdir -p latest
+      cp -r ../build/docs/. ./latest/
+      git add latest/*
     fi
 
-    ./gradlew allDocs || EXIT_STATUS=$?
+    majorVersion=${version:0:4}
+    majorVersion="${majorVersion}x"
 
-    git config --global user.name "$GIT_NAME"
-    git config --global user.email "$GIT_EMAIL"
-    git config --global credential.helper "store --file=~/.git-credentials"
-    echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+    mkdir -p "$version"
+    cp -r ../build/docs/. "./$version/"
+    git add "$version/*"
 
-	git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
-	cd gh-pages
+    mkdir -p "$majorVersion"
+    cp -r ../build/docs/. "./$majorVersion/"
+    git add "$majorVersion/*"
 
-	# If this is the master branch then update the snapshot
-	if [[ $TRAVIS_BRANCH == 'master' ]]; then
-		mkdir -p snapshot
-		cp -r ../build/docs/. ./snapshot/
+  fi
 
-		git add snapshot/*
-	fi
-
-    # If there is a tag present then this becomes the latest
-    if [[ -n $TRAVIS_TAG ]]; then
-        version="$TRAVIS_TAG"
-        version=${version:1}
-		milestone=${version:5}
-		if [[ -n $milestone ]]; then
-            mkdir -p latest
-            cp -r ../build/docs/. ./latest/
-            git add latest/*
-		fi
-
-        majorVersion=${version:0:4}
-        majorVersion="${majorVersion}x"
-
-        mkdir -p "$version"
-        cp -r ../build/docs/. "./$version/"
-        git add "$version/*"
-
-        mkdir -p "$majorVersion"
-        cp -r ../build/docs/. "./$majorVersion/"
-        git add "$majorVersion/*"
-
-    fi
-
-    git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-    git push origin HEAD
-    cd ..
-    rm -rf gh-pages
+  git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+  git push origin HEAD
+  cd ..
+  rm -rf gh-pages
 fi
 
 exit $EXIT_STATUS
