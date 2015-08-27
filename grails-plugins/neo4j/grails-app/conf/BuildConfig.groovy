a81cd007e7edc76edccc584c27a929434a7f6e5e
Graeme Rocher <graeme.rocher@gmail.com>
2015-08-27 16:05:06 +0200

    fix neo4j dependencies

--- grails-plugins/neo4j/grails-app/conf/BuildConfig.groovy
+++ grails-plugins/neo4j/grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
+grails.project.dependency.resolver="maven"
 grails.project.dependency.resolution = {
 
     inherits("global") {
@@ -13,11 +14,10 @@ grails.project.dependency.resolution = {
 
     repositories {
         inherits true
-
+        mavenLocal()
         grailsPlugins()
         grailsHome()
-        grailsCentral()
-        mavenLocal()
+        grailsCentral()        
         mavenCentral()
 
         mavenRepo "http://m2.neo4j.org/content/repositories/releases/"
@@ -28,6 +28,11 @@ grails.project.dependency.resolution = {
 
     dependencies {
 
+
+        def datastoreVersion = "3.1.6.BUILD-SNAPSHOT"
+        def neo4jDatastoreVersion = "2.0.0-SNAPSHOT"
+//        def neo4jDatastoreVersion = "2.0.0-M02"
+        def seleniumVersion = "2.40.0"
         def excludes = {
             excludes "slf4j-simple", "persistence-api", "commons-logging", "jcl-over-slf4j", "slf4j-api", "jta"
             excludes "spring-core", "spring-beans", "spring-aop", "spring-asm","spring-webmvc","spring-tx", "spring-context", "spring-web", "log4j", "slf4j-log4j12"
@@ -36,25 +41,22 @@ grails.project.dependency.resolution = {
             excludes group:"org.grails", name:'grails-test'
             excludes group:'xml-apis', name:'xml-apis'
             excludes 'ehcache-core'
-            transitive = false
         }
 
-        def datastoreVersion = "2.0.7.RELEASE"
-        def neo4jDatastoreVersion = "2.0.0-SNAPSHOT"
-//        def neo4jDatastoreVersion = "2.0.0-M02"
-        def seleniumVersion = "2.40.0"
-
-        compile("org.grails:grails-datastore-gorm-neo4j:$neo4jDatastoreVersion",
-                "org.grails:grails-datastore-gorm-plugin-support:$datastoreVersion",
+        compile ("org.grails:grails-datastore-gorm-neo4j:$neo4jDatastoreVersion",excludes)
+        compile("org.grails:grails-datastore-gorm-plugin-support:$datastoreVersion",
                 "org.grails:grails-datastore-gorm:$datastoreVersion",
                 "org.grails:grails-datastore-core:$datastoreVersion",
-                "org.grails:grails-datastore-web:$datastoreVersion", excludes)
-
-        test("org.grails:grails-datastore-gorm-test:$datastoreVersion",
-             "org.grails:grails-datastore-simple:$datastoreVersion", exlcudes)
+                "org.grails:grails-datastore-simple:$datastoreVersion",
+                "org.grails:grails-datastore-web:$datastoreVersion",excludes)        
 
         compile('org.neo4j:neo4j-community:2.0.3')
+
         compile "org.neo4j:neo4j-jdbc:2.0.2"
+        runtime 'org.apache.httpcomponents:httpcore:4.3'
+        runtime 'org.apache.httpcomponents:httpclient:4.3.1'
+        runtime 'org.apache.httpcomponents:httpmime:4.3.1'
+
 
         test "org.gebish:geb-spock:0.9.2"
         test "org.seleniumhq.selenium:selenium-support:$seleniumVersion"
@@ -74,7 +76,10 @@ grails.project.dependency.resolution = {
         runtime(":jquery:1.11.0.1", ":resources:1.2.7") {
             export = false
         }
-        build(":release:3.0.1",  ":rest-client-builder:1.0.3",":tomcat:7.0.50") {
+        build(":tomcat:7.0.50") {
+            export = false
+        }
+        build(":release:3.0.1",  ":rest-client-builder:1.0.3") {
             export = false
         }
         test(":spock:0.7", ":geb:0.9.2") {
