4d42d7e1cb73b02306903a024b817a31b6c357d4
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-09-08 12:53:00 -0500

    Grails 2.5.1

--- grails-plugins/neo4j/grails-app/conf/BuildConfig.groovy
+++ grails-plugins/neo4j/grails-app/conf/BuildConfig.groovy
@@ -1,9 +1,22 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.dependency.resolver="maven"
 
-grails.project.dependency.resolver="maven"
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
 
     inherits("global") {
@@ -26,7 +39,6 @@ grails.project.dependency.resolution = {
         //mavenRepo 'http://m2.neo4j.org/releases'
         mavenRepo "https://repo.grails.org/grails/plugins"
     }
-
     dependencies {
 
 
@@ -75,13 +87,13 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime(":jquery:1.11.0.1", ":resources:1.2.7") {
+        runtime(":jquery:1.11.1", ":resources:1.2.14") {
             export = false
         }
         build(":tomcat:7.0.50") {
             export = false
         }
-        build(":release:3.0.1",  ":rest-client-builder:1.0.3") {
+        build(":release:3.1.1",  ":rest-client-builder:2.1.1") {
             export = false
         }
 /*        test(":spock:0.7", ":geb:0.9.2") {
