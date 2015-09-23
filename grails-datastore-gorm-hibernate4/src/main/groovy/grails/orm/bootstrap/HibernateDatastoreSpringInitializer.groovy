1378b83204d432393e7afeb647738b25b60821e5
graemerocher <graeme.rocher@gmail.com>
2015-09-23 16:34:04 +0200

    fix compilation error

--- grails-datastore-gorm-hibernate4/src/main/groovy/grails/orm/bootstrap/HibernateDatastoreSpringInitializer.groovy
+++ grails-datastore-gorm-hibernate4/src/main/groovy/grails/orm/bootstrap/HibernateDatastoreSpringInitializer.groovy
@@ -23,6 +23,7 @@ import groovy.transform.CompileStatic
 import groovy.util.logging.Commons
 import org.grails.config.NavigableMap
 import org.grails.config.PropertySourcesConfig
+import org.grails.datastore.mapping.engine.event.DatastoreInitializedEvent
 import org.grails.orm.hibernate.*
 import org.grails.orm.hibernate.cfg.GrailsDomainBinder
 import org.grails.orm.hibernate.cfg.HibernateUtils
