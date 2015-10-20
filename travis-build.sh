e3f012bad561e0d384b10b6c54d53ced598e648e
graemerocher <graeme.rocher@gmail.com>
2015-10-20 12:14:03 +0200

    Tweak travis memory

--- travis-build.sh
+++ travis-build.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 EXIT_STATUS=0
 
+./gradlew --stop
+
 case "$GORM_IMPL"  in
     hibernate)
-        ./gradlew grails-datastore-gorm-hibernate:test || EXIT_STATUS=$?
+        ./gradlew grails-datastore-gorm-hibernate:test -no-daemon  || EXIT_STATUS=$?
         ;;
     hibernate4)
-        ./gradlew grails-datastore-gorm-hibernate4:test --stacktrace || EXIT_STATUS=$?
+        ./gradlew grails-datastore-gorm-hibernate4:test -no-daemon  --stacktrace || EXIT_STATUS=$?
         ;;
     mongodb)
-        ./gradlew grails-datastore-gorm-mongodb:test --stacktrace || EXIT_STATUS=$?
+        ./gradlew grails-datastore-gorm-mongodb:test -no-daemon --stacktrace || EXIT_STATUS=$?
         ;;
     redis)
-        ./gradlew grails-datastore-gorm-redis:test || EXIT_STATUS=$?
+        ./gradlew grails-datastore-gorm-redis:test -no-daemon  || EXIT_STATUS=$?
         ;;
     cassandra)
         # wait for Cassandra to start up
         sleep 5
-        ./gradlew grails-datastore-gorm-cassandra:test || EXIT_STATUS=$?
+        ./gradlew grails-datastore-gorm-cassandra:test -no-daemon  || EXIT_STATUS=$?
         ;;
     neo4j)
-        ./gradlew --stop
         ./gradlew grails-datastore-gorm-neo4j:test -no-daemon  || EXIT_STATUS=$?
         ;;
     restclient)
-        ./gradlew grails-datastore-gorm-rest-client:test || EXIT_STATUS=$?
+        ./gradlew grails-datastore-gorm-rest-client:test -no-daemon  || EXIT_STATUS=$?
         ;;
     *)
         ./gradlew testClasses || EXIT_STATUS=$?
