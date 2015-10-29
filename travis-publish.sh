d1fcdc4f99fd7aac0f071c93c2e40e684e9c090a
graemerocher <graeme.rocher@gmail.com>
2015-10-29 16:51:11 +0100

    publish documentation snapshot

--- travis-publish.sh
+++ travis-publish.sh
@@ -44,13 +44,13 @@ if [[ $TRAVIS_REPO_SLUG == "grails/grails-data-mapping" && $TRAVIS_PULL_REQUEST 
       git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
       cd gh-pages
 
-#      # If this is the master branch then update the snapshot
-#      if [[ $TRAVIS_BRANCH == 'master' ]]; then
-#        mkdir -p snapshot
-#        cp -r ../build/docs/. ./snapshot/
-#
-#        git add snapshot/*
-#      fi
+      # If this is the master branch then update the snapshot
+      if [[ $TRAVIS_BRANCH == 'master' ]]; then
+        mkdir -p snapshot
+        cp -r ../build/docs/. ./snapshot/
+
+        git add snapshot/*
+      fi
 
       # If there is a tag present then this becomes the latest
       if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
