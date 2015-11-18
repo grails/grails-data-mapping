5b46f115dc312ac601a64cfe84c11c6468323311
graemerocher <graeme.rocher@gmail.com>
2015-11-18 15:28:51 +0100

    Remove dead or deprecated implementations

--- grails-datastore-gorm-jpa/src/test/groovy/grails/gorm/tests/OptimisticLockingSpec.groovy
+++
@@ -1,9 +0,0 @@
-package grails.gorm.tests
-
-/**
- * @author Burt Beckwith
- */
-class OptimisticLockingSpec {
-    // TODO implement optimistic locking for JPA and delete this
-    void testNothing() {}
-}
