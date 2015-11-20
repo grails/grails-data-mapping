fb9a913ecf338d904eb31537bc52a668b22d2c5a
graemerocher <graeme.rocher@gmail.com>
2015-11-20 14:26:30 +0100

    Attempt to fix Travis publishing

--- travis-publish.sh
+++ travis-publish.sh
@@ -27,14 +27,23 @@ if [[ $TRAVIS_REPO_SLUG == "grails/grails-data-mapping" && $TRAVIS_PULL_REQUEST 
   gpg --keyserver keyserver.ubuntu.com --recv-key $SIGNING_KEY
   if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
     # for releases we upload to Bintray and Sonatype OSS
-    ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish uploadArchives bintrayUpload  || EXIT_STATUS=$?
+    ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish bintrayUpload -x grails2-plugins/neo4j:publish -x grails2-plugins/hibernate4:publish -x grails2-plugins/mongodb:publish || EXIT_STATUS=$?
+    if [[ $EXIT_STATUS -eq 0 ]]; then
+    ./gradlew grails2-plugins/neo4j:publish grails2-plugins/hibernate4:publish grails2-plugins/mongodbo:publish || EXIT_STATUS=$?
+    ./gradlew --stop
+    fi
+
   else
     # for snapshots only to repo.grails.org
-    ./gradlew publish || EXIT_STATUS=$?
+    ./gradlew publish -x grails2-plugins/neo4j:publish -x grails2-plugins/hibernate4:publish -x grails2-plugins/mongodb:publish || EXIT_STATUS=$?
+    ./gradlew --stop
+    if [[ $EXIT_STATUS -eq 0 ]]; then
+    ./gradlew grails2-plugins/neo4j:publish grails2-plugins/hibernate4:publish grails2-plugins/mongodbo:publish || EXIT_STATUS=$?
+    ./gradlew --stop
+    fi
   fi
 
   if [[ $EXIT_STATUS -eq 0 ]]; then
-      ./gradlew --stop
       echo "Trigger Travis Functional Test build"
       ./gradlew travisciTrigger -i
       ./gradlew --stop
