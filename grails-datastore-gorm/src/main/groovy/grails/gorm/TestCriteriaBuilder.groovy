983f64a70bc61617a279d35dd6b3507e95c48758
graemerocher <graeme.rocher@gmail.com>
2015-11-20 16:08:24 +0100

    Restore class for binary compatibility purposes

---
+++ grails-datastore-gorm/src/main/groovy/grails/gorm/TestCriteriaBuilder.groovy
@@ -0,0 +1,13 @@
+package grails.gorm
+
+import groovy.transform.InheritConstructors
+
+/**
+ * A CriteriaBuilder for unit testing
+ *
+ * @deprecated Here for compatibility. Will be removed in a future version of Grails
+ */
+@InheritConstructors
+@Deprecated
+class TestCriteriaBuilder extends CriteriaBuilder{
+}
