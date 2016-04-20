6dccdea1ce86d595a07d8965124aa47a1782da0a
graemerocher <graeme.rocher@gmail.com>
2016-04-20 10:48:25 +0200

    Fix SchemaExportCommand for Hibernate 5.1 changes

--- grails-plugins/hibernate5/src/main/groovy/grails/plugin/hibernate/commands/SchemaExportCommand.groovy
+++ grails-plugins/hibernate5/src/main/groovy/grails/plugin/hibernate/commands/SchemaExportCommand.groovy
@@ -21,8 +21,13 @@ import grails.util.Environment
 import groovy.transform.CompileStatic
 import org.grails.build.parsing.CommandLine
 import org.grails.orm.hibernate.HibernateMappingContextSessionFactoryBean
+import org.grails.orm.hibernate.cfg.HibernateMappingContextConfiguration
+import org.hibernate.boot.MetadataBuilder
+import org.hibernate.boot.MetadataSources
+import org.hibernate.boot.spi.MetadataImplementor
+import org.hibernate.service.ServiceRegistry
 import org.hibernate.tool.hbm2ddl.SchemaExport as HibernateSchemaExport
-
+import org.hibernate.tool.schema.TargetType
 /**
  * Adds a schema-export command
  *
@@ -58,18 +63,23 @@ class SchemaExportCommand implements ApplicationCommand {
         file.parentFile.mkdirs()
 
         def sessionFactory = applicationContext.getBean('&sessionFactory' + datasourceSuffix, HibernateMappingContextSessionFactoryBean)
-        def configuration = sessionFactory.configuration
+        HibernateMappingContextConfiguration configuration = (HibernateMappingContextConfiguration)sessionFactory.configuration
 
-        def schemaExport = new HibernateSchemaExport(configuration, sessionFactory.dataSource.connection)
+
+        def serviceRegistry = configuration.serviceRegistry
+        def metadata = buildMetadata(executionContext, serviceRegistry)
+
+        def schemaExport = new HibernateSchemaExport()
                 .setHaltOnError(true)
                 .setOutputFile(file.path)
                 .setDelimiter(';')
+
 
         String action = export ? "Exporting" : "Generating script to ${file.path}"
         String ds = argsMap.datasource ? "for DataSource '$argsMap.datasource'" : "for the default DataSource"
         println "$action in environment '${Environment.current.name}' $ds"
 
-        schemaExport.execute stdout, export, false, false
+        schemaExport.execute(EnumSet.of(TargetType.SCRIPT, TargetType.STDOUT), HibernateSchemaExport.Action.CREATE, metadata, serviceRegistry)
 
         if (schemaExport.exceptions) {
             def e = (Exception)schemaExport.exceptions[0]
@@ -77,5 +87,14 @@ class SchemaExportCommand implements ApplicationCommand {
             return false
         }
         return true
+    }
+
+
+    protected MetadataImplementor buildMetadata(
+            ExecutionContext context,
+            ServiceRegistry serviceRegistry) throws Exception {
+        final MetadataSources metadataSources = new MetadataSources( serviceRegistry );
+        final MetadataBuilder metadataBuilder = metadataSources.getMetadataBuilder();
+        return (MetadataImplementor) metadataBuilder.build();
     }
 }
