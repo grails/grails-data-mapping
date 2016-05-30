da070d836533bc5c80a6023d02dded45eba79d2f
Graeme Rocher <graemerocher@Graemes-iMac.local>
2016-05-30 09:22:19 +0200

    RxGORM - fix mongodb host/port settings

--- grails-datastore-gorm-rx-mongodb/src/test/groovy/grails/gorm/rx/mongodb/config/MongoClientSettingsBuilderSpec.groovy
+++ grails-datastore-gorm-rx-mongodb/src/test/groovy/grails/gorm/rx/mongodb/config/MongoClientSettingsBuilderSpec.groovy
@@ -1,6 +1,7 @@
 package grails.gorm.rx.mongodb.config
 
 import com.mongodb.ReadPreference
+import com.mongodb.ServerAddress
 import com.mongodb.async.client.MongoClientSettings
 import org.grails.datastore.rx.mongodb.config.MongoClientSettingsBuilder
 import org.springframework.core.env.MapPropertySource
@@ -14,6 +15,8 @@ class MongoClientSettingsBuilderSpec extends Specification {
         when:"using a property resolver"
         StandardEnvironment resolver = new StandardEnvironment()
         Map myMap = ['grails.mongodb.options.readPreference': 'secondary',
+                     'grails.mongodb.host': 'mycompany',
+                     'grails.mongodb.port': '1234',
                      'grails.mongodb.username': 'foo',
                      'grails.mongodb.password': 'bar',
                      'grails.mongodb.options.clusterSettings.maxWaitQueueSize': '10']
@@ -23,6 +26,7 @@ class MongoClientSettingsBuilderSpec extends Specification {
         def clientSettings = builder.build()
 
         then:"The settings are correct"
+        clientSettings.clusterSettings.hosts.contains(new ServerAddress("mycompany", 1234))
         clientSettings.readPreference == ReadPreference.secondary()
         clientSettings.credentialList.size() == 1
         clientSettings.clusterSettings.maxWaitQueueSize == 10
