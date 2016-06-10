ece4bf2b29fa6fbab094b9ef5e61c29df83cea04
graemerocher <graeme.rocher@gmail.com>
2016-06-10 18:06:48 +0200

    Remove deprecated implementations

--- grails-datastore-gorm-riak/src/test/groovy/grails/gorm/tests/OptimisticLockingSpec.groovy
+++
@@ -1,9 +0,0 @@
-package grails.gorm.tests
-
-/**
- * @author Burt Beckwith
- */
-class OptimisticLockingSpec {
-    // TODO implement optimistic locking for Gemfire and delete this
-    void testNothing() {}
-}
