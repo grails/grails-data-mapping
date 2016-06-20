8685994a69d7c6ebc34790c9da54f8b5142d7b9f
graemerocher <graeme.rocher@gmail.com>
2016-06-20 16:43:33 +0200

    ObservableProxy changes
    If the resolved object is an instance use that, otherwise assume it is the id

--- grails-datastore-gorm-rx/src/main/groovy/org/grails/datastore/rx/proxy/QueryObservableProxyMethodHandler.java
+++ grails-datastore-gorm-rx/src/main/groovy/org/grails/datastore/rx/proxy/QueryObservableProxyMethodHandler.java
@@ -2,7 +2,6 @@ package org.grails.datastore.rx.proxy;
 
 import org.grails.datastore.mapping.query.Query;
 import org.grails.datastore.rx.RxDatastoreClient;
-import org.grails.datastore.rx.internal.RxDatastoreClientImplementor;
 import org.grails.datastore.rx.query.QueryState;
 import org.grails.datastore.rx.query.RxQuery;
 import org.slf4j.Logger;
@@ -13,19 +12,19 @@ import rx.functions.Func1;
 import java.io.Serializable;
 
 /**
- * A proxy {@link javassist.util.proxy.MethodHandler} that uses a query to resolve the id of the the target
+ * A proxy {@link javassist.util.proxy.MethodHandler} that uses a query to resolve the target using a query
  *
  * @author Graeme Rocher
  * @since 6.0
  */
-public class IdQueryObservableProxyMethodHandler extends AbstractObservableProxyMethodHandler {
+public class QueryObservableProxyMethodHandler extends AbstractObservableProxyMethodHandler {
     private static final Logger LOG = LoggerFactory.getLogger(IdQueryObservableProxyMethodHandler.class);
 
     private final Query query;
     protected final Observable observable;
     protected Serializable proxyKey;
 
-    public IdQueryObservableProxyMethodHandler(Class proxyClass, Query query, QueryState queryState, RxDatastoreClient client) {
+    public QueryObservableProxyMethodHandler(Class proxyClass, Query query, QueryState queryState, RxDatastoreClient client) {
         super(proxyClass, query.getEntity().getJavaClass(), queryState, client);
         this.query = query;
         this.observable = resolveObservable();
@@ -36,28 +35,6 @@ public class IdQueryObservableProxyMethodHandler extends AbstractObservableProxy
         query.projections().id();
         Observable queryResult = ((RxQuery) query).singleResult();
 
-        queryResult = queryResult.switchMap(new Func1<Object, Observable>() {
-            @Override
-            public Observable call(Object id) {
-                if(type.isInstance(id)) {
-                    return Observable.just(id);
-                }
-                else if(id != null) {
-                    Serializable key = (Serializable) id;
-                    proxyKey = key;
-                    Object loadedEntity = queryState.getLoadedEntity(type, key);
-                    if(loadedEntity != null) {
-                        return Observable.just(loadedEntity);
-                    }
-                    else {
-                        return ((RxDatastoreClientImplementor)client).get(type, key, queryState);
-                    }
-                }
-                else {
-                    return null;
-                }
-            }
-        });
         return queryResult.map(new Func1() {
             @Override
             public Object call(Object o) {
