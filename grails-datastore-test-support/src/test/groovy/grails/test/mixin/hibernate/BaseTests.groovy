853245602f3df66851cbb61a163bfd2ec61392e7
Graeme Rocher <graeme.rocher@gmail.com>
2016-07-15 11:12:27 +0200

    remove test mixin classes from project

--- grails-datastore-test-support/src/test/groovy/grails/test/mixin/hibernate/BaseTests.groovy
+++
@@ -1,9 +0,0 @@
-package grails.test.mixin.hibernate
-
-import grails.test.mixin.TestMixin
-import grails.test.mixin.gorm.Domain
-
-@Domain(Person)
-@TestMixin(HibernateTestMixin)
-class BaseTests {
-}
