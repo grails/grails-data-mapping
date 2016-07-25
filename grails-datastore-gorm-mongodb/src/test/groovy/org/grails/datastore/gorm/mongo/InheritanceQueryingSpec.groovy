27113bbe02680a48ec7f976192922b1e83a83fa1
James Kleeh <james.kleeh@gmail.com>
2016-07-25 10:58:20 -0400

    Add entries in persistentEntitiesByDiscriminator for all parent classes instead of only root. Update mongo to include multiple class names in queries if multiple children exist

--- grails-datastore-gorm-mongodb/src/test/groovy/org/grails/datastore/gorm/mongo/InheritanceQueryingSpec.groovy
+++ grails-datastore-gorm-mongodb/src/test/groovy/org/grails/datastore/gorm/mongo/InheritanceQueryingSpec.groovy
@@ -8,6 +8,7 @@ class InheritanceQueryingSpec extends GormDatastoreSpec {
     def cleanup() {
         A.get("id")?.delete(flush:true)
         B.get("id")?.delete(flush:true)
+        C.get("childId")?.delete(flush: true)
     }
 
     def setup() {
@@ -15,16 +16,24 @@ class InheritanceQueryingSpec extends GormDatastoreSpec {
         b.id = "id"
         b.prop = "value"
         b.save(failOnError:true, flush:true)
+
+        C c = new C()
+        c.id = "childId"
+        c.prop = "childValue"
+        c.name = "childName"
+        c.save(failOnError:true, flush:true)
     }
 
     def "Test collection and count sizes"() {
-        expect: "Both collections have 1"
-            B.collection.count()==1
-            A.collection.count()==1
+        expect: "Collections to have 2 documents"
+            C.collection.count()==2
+            B.collection.count()==2
+            A.collection.count()==2
 
-        and: "Both counts have 1"
-            B.count()==1
-            A.count()==1
+        and: "A/B have 2, C has 1"
+            C.count()==1
+            B.count()==2
+            A.count()==2
     }
 
     def "Test listing"() {
@@ -32,15 +41,24 @@ class InheritanceQueryingSpec extends GormDatastoreSpec {
             def bList = B.list()
 
         then:
-            bList.size()==1
+            bList.size()==2
             bList[0].id=="id"
+            bList[1].id=="childId"
 
         when:
             def aList = A.list()
 
         then:
-            aList.size()==1
+            aList.size()==2
             aList[0].id=="id"
+            aList[1].id=="childId"
+
+        when:
+            def cList = C.list()
+
+        then:
+            cList.size()==1
+            cList[0].id=="childId"
     }
 
     def "Test getting"() {
@@ -55,24 +73,66 @@ class InheritanceQueryingSpec extends GormDatastoreSpec {
             def a = A.get("id")
 
         then:
-            a?.id=="id"
+            a.id=="id"
             a instanceof B
             a.hasProperty("prop")
+
+        when:
+            b = B.get("childId")
+
+        then:
+            b.id=="childId"
+            b instanceof C
+            b.prop=="childValue"
+            b.hasProperty("name")
+
+        when:
+            a = A.get("childId")
+
+        then:
+            a.id=="childId"
+            a instanceof C
+            a.hasProperty("prop")
+            a.hasProperty("name")
+
+        when:
+            def c = C.get("childId")
+
+        then:
+            c.id=="childId"
+            c.prop=="childValue"
+            c.name=="childName"
     }
 
     def "Access prop through mongo"() {
         when:
-            def jsonString = (A.collection.findOne(_id:"id")).toString()
+            def jsonString = (A.collection.findOne(_id: "id")).toString()
 
         then:
             jsonString.contains("id")
             jsonString.contains("prop")
 
+        when:
+            jsonString = (B.collection.findOne(_id: "id")).toString()
+
+        then:
+            jsonString.contains("id")
+            jsonString.contains("prop")
+
+        when:
+            jsonString = (B.collection.findOne(_id: "childId")).toString()
+
+        then:
+            jsonString.contains("id")
+            jsonString.contains("prop")
+            jsonString.contains("name")
     }
+
+
 
     @Override
     List getDomainClasses() {
-        [A,B]
+        [A,B,C]
     }
 }
 
@@ -93,4 +153,13 @@ class B extends A {
     static constraints = {
     }
     String prop
+}
+
+@Entity
+class C extends B {
+    static mapWith = "mongo"
+
+    static constraints = {
+    }
+    String name
 }
