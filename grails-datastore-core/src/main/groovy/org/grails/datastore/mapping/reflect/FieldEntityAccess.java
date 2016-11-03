b66633940d75020ffc0c37c6c3606a4f4cb9ddad
graemerocher <graeme.rocher@gmail.com>
2016-11-03 13:04:12 +0100

    Make error message less confusing

--- grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/reflect/FieldEntityAccess.java
+++ grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/reflect/FieldEntityAccess.java
@@ -424,7 +424,7 @@ public class FieldEntityAccess implements EntityAccess {
                 try {
                     field.set(object, value);
                 } catch (Throwable e) {
-                    throw new IllegalArgumentException("Cannot write field ["+field+"] to object ["+object+"] of type ["+object.getClass()+"]");
+                    throw new IllegalArgumentException("Cannot set field ["+field.getName()+"] of object ["+object+"] for value ["+value+"] of type ["+value.getClass().getName()+"]");
                 }
             }
         }
