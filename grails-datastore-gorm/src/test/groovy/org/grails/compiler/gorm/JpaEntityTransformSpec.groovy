3126d19fd0e55f749b4a80c8bb63e548129a2980
graemerocher <graeme.rocher@gmail.com>
2017-01-02 14:41:56 +0100

    fix failing test

--- grails-datastore-gorm/src/test/groovy/org/grails/compiler/gorm/JpaEntityTransformSpec.groovy
+++ grails-datastore-gorm/src/test/groovy/org/grails/compiler/gorm/JpaEntityTransformSpec.groovy
@@ -46,7 +46,7 @@ class Customer {
         customerClass.getDeclaredMethod("getId").returnType == Long
         customerClass.getDeclaredMethod("getId").getAnnotation(Transient)
         cpf.getPropertyDescriptor(GormProperties.IDENTITY)
-        !cpf.getPropertyDescriptor(GormProperties.VERSION)
+        cpf.getPropertyDescriptor(GormProperties.VERSION)
         customerClass.getDeclaredMethod('addToRelated', Object)
         customerClass.getDeclaredMethod('removeFromRelated', Object)
     }
