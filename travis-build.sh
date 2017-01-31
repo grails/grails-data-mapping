aab476fbd98f954c88328a22f540284a3016b005
graemerocher <graeme.rocher@gmail.com>
2017-01-31 14:51:05 +0100

    Fix publishing

--- travis-build.sh
+++ travis-build.sh
@@ -17,12 +17,14 @@ else
     fi
 fi
 
-if [[ $EXIT_STATUS -eq 0 ]]; then
-    ./gradlew --stop
-    ./travis-publish.sh || EXIT_STATUS=$?
+if [[ $EXIT_STATUS -eq 0 && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+    if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == '6.0.x' ]]; then
+        ./gradlew --stop
+        ./travis-publish.sh || EXIT_STATUS=$?
+    fi
 fi
 
-exit $EXIT_STATUS
 
+exit $EXIT_STATUS
 
 
