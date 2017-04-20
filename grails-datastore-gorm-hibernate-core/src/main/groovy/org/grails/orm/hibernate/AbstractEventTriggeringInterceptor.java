44b03a0d12cb7cfe54b9c5c989bf78b4e83d5c9b
graemerocher <graeme.rocher@gmail.com>
2017-04-20 11:32:57 +0200

    Revert "The flush mode was not being propagated to Hibernate correctly and was always AUTO."
    This reverts commit 79469da36398de4549819e618430a58338a616ea.

--- grails-datastore-gorm-hibernate-core/src/main/groovy/org/grails/orm/hibernate/AbstractEventTriggeringInterceptor.java
+++ grails-datastore-gorm-hibernate-core/src/main/groovy/org/grails/orm/hibernate/AbstractEventTriggeringInterceptor.java
@@ -5,7 +5,7 @@ import org.grails.orm.hibernate.event.listener.AbstractHibernateEventListener;
 /**
  * <p>Invokes closure events on domain entities such as beforeInsert, beforeUpdate and beforeDelete.
  *
- * @deprecated Renamed to avoid confusion with {@link org.grails.orm.hibernate.support.ClosureEventTriggeringInterceptor}. Replaced by {@link AbstractHibernateEventListener}
+ * @deprecated Renamed to avoid confusiong with {@link org.grails.orm.hibernate.support.ClosureEventTriggeringInterceptor}. Replaced by {@link AbstractHibernateEventListener}
  */
 @Deprecated
 public abstract class AbstractEventTriggeringInterceptor extends AbstractHibernateEventListener {
