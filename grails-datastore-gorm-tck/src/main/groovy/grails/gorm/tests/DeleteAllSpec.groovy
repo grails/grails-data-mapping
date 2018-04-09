86bb567bde3058941116892f177a37e88fc9ad2b
benrhine <rhine.ben@gmail.com>
2018-04-09 10:47:03 -0400

    improvement for 10680

--- grails-datastore-gorm-tck/src/main/groovy/grails/gorm/tests/DeleteAllSpec.groovy
+++ grails-datastore-gorm-tck/src/main/groovy/grails/gorm/tests/DeleteAllSpec.groovy
@@ -34,4 +34,35 @@ class DeleteAllSpec extends GormDatastoreSpec {
         then:
             total == 0
     }
+
+    def "Test that many objects can be deleted at once using multiple arguments and flushes"() {
+        given:
+            def bob = new Person(firstName:"Bob", lastName:"Builder").save(flush: true)
+            def fred = new Person(firstName:"Fred", lastName:"Flintstone").save(flush: true)
+            def joe = new Person(firstName:"Joe", lastName:"Doe").save(flush: true)
+            Person.deleteAll(flush: true, bob, fred, joe)
+
+        when:
+            def total = Person.count()
+            then:
+            total == 0
+    }
+    def "Test that many objects can be deleted using an iterable and flushes"() {
+        given:
+            def bob = new Person(firstName:"Bob", lastName:"Builder").save(flush: true)
+            def fred = new Person(firstName:"Fred", lastName:"Flintstone").save(flush: true)
+            def joe = new Person(firstName:"Joe", lastName:"Doe").save(flush: true)
+
+            Vector<Person> people = new Vector<Person>()
+            people.add(bob)
+            people.add(fred)
+            people.add(joe)
+
+            Person.deleteAll(flush: true, people)
+
+        when:
+            def total = Person.count()
+            then:
+            total == 0
+    }
 }
