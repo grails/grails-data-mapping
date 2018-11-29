81e7cefb5db9973ff41fbca7dd342ce3a470b341
Iván López <lopezi@objectcomputing.com>
2018-11-29 16:26:42 +0100

    Clean travis-build.sh script and only publish when executing with jdk8

--- travis-build.sh
+++ travis-build.sh
@@ -4,27 +4,25 @@ EXIT_STATUS=0
 ./gradlew --stop
 
 if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
+
+    # Only JDK8 execution will publish the release
+    if [ "${TRAVIS_JDK_VERSION}" == "openjdk11" ] ; then
+        exit $EXIT_STATUS
+    fi
+
     echo "Tagged Release Skipping Tests for Publish"
     ./travis-publish.sh || EXIT_STATUS=$?
 else
     ./gradlew --no-daemon compileTestGroovy || EXIT_STATUS=$?
-    ./gradlew --stop
-    if [[ $EXIT_STATUS -eq 0 ]]; then
-        ./gradlew --no-daemon  compileTestGroovy|| EXIT_STATUS=$?
-    fi
     if [[ $EXIT_STATUS -eq 0 ]]; then
         ./gradlew --no-daemon --refresh-dependencies check || EXIT_STATUS=$?
         if [[ $EXIT_STATUS -eq 0 && $TRAVIS_PULL_REQUEST == 'false' ]]; then
             echo "Travis Branch $TRAVIS_BRANCH"
-            if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' ]]; then
-
+            if ([[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' ]] && [[ "${TRAVIS_JDK_VERSION}" != "openjdk11" ]]); then
                 ./travis-publish.sh || EXIT_STATUS=$?
             fi
         fi
     fi
 fi
 
-
 exit $EXIT_STATUS
-
-
