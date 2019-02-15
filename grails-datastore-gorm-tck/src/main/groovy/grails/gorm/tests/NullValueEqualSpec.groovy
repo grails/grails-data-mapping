6989955e4940419844f4b41265c8d61bbfdb0904
dng <chartgizmo@gmail.com>
2019-02-16 00:01:50 +0100

    null value support for dynamic finders https://github.com/grails/grails-data-mapping/issues/1182

--- grails-datastore-gorm-tck/src/main/groovy/grails/gorm/tests/NullValueEqualSpec.groovy
+++ grails-datastore-gorm-tck/src/main/groovy/grails/gorm/tests/NullValueEqualSpec.groovy
@@ -4,13 +4,15 @@ class NullValueEqualSpec extends GormDatastoreSpec {
 
   void "test null value in equal and not equal"() {
     when:
-    new TestEntity(name:"Fred").save(failOnError: true)
+    new TestEntity(name:"Fred", age: null).save(failOnError: true)
     new TestEntity(name:"Bob", age: 11).save(failOnError: true)
-    new TestEntity(name:"Jack").save(flush:true, failOnError: true)
+    new TestEntity(name:"Jack", age: null).save(flush:true, failOnError: true)
 
     then:
-    TestEntity.countByAge(null) == 2
     TestEntity.countByAge(11) == 1
+    TestEntity.findAllByAge(null).size() == 2
+    TestEntity.countByAge(null) == 2
+
     TestEntity.countByAgeNotEqual(11) == 2
     TestEntity.countByAgeNotEqual(null) == 1
   }
