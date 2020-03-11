fff33d800923db1a6c3f1103576f61034a808944
Puneet Behl <behl4485@gmail.com>
2020-03-11 22:11:51 +0530

    Updated Groovy issue reference in the TODO

--- grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/finders/DynamicFinder.java
+++ grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/finders/DynamicFinder.java
@@ -466,7 +466,7 @@ public abstract class DynamicFinder extends AbstractFinder implements QueryBuild
      * @param query The query
      * @param argMap The query arguments
      */
-    //TODO: Change {code}Class<? extends Object>{code} to {class<?>} once GROOVY-XXX is fixed.
+    //TODO: Change {code}Class<? extends Object>{code} to {class<?>} once GROOVY-9460 is fixed.
     public static void populateArgumentsForCriteria(Class<? extends Object> targetClass, Query query, Map argMap) {
         if (argMap == null) {
             return;
