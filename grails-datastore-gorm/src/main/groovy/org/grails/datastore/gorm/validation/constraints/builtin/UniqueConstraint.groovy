6d0872401ea1d5a7d0fda395ce6f07ce3b90c38a
Juan José GIL <matero@gmail.com>
2020-03-25 19:37:14 -0300

    Verifying that target is not transient before accessing its id

--- grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/validation/constraints/builtin/UniqueConstraint.groovy
+++ grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/validation/constraints/builtin/UniqueConstraint.groovy
@@ -138,7 +138,13 @@ class UniqueConstraint extends AbstractConstraint {
             if (shouldValidate) {
                 def existingId = detachedCriteria.get()
                 if (existingId != null) {
-                    def targetId = reflector.getIdentifier(target)
+                    // We are merely verifying that the object is not transient here
+                    def targetId
+                    if (proxyHandler.isProxy(target)) {
+                        targetId = proxyHandler.getIdentifier(target)
+                    } else {
+                        targetId = reflector.getIdentifier(target)
+                    }
                     if (targetId != existingId) {
                         def args = [constraintPropertyName, constraintOwningClass, propertyValue] as Object[]
                         rejectValue(target, errors, "unique", args, getDefaultMessage("default.not.unique.message"))
