c384d49fdca4cdc7ab6fd8c2b82e795fb8da4ad7
Graeme Rocher <graeme.rocher@gmail.com>
2020-04-21 09:04:50 +0000

    Updating docs for Travis build: https://travis-ci.org/grails/gorm-docs/builds/677605369

---
+++ 7.1.0.M1/js/hipchatwidget.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".hipchatpanel").hipChatPanel({url:"https://www.hipchat.com/gvUjrMN8d",timezone:"CET",buttonTitle:"I understand, please let me in!",buttonClasses:"btn btn-default sign-in-fa-icon"})});
