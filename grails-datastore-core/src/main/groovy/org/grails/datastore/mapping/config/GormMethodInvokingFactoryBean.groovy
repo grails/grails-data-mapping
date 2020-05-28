1b56873827ea6f0ddbaa4c4bfd13677d7e134b06
Puneet Behl <behl4485@gmail.com>
2020-05-29 01:13:02 +0530

    grails/gorm-hibernate5#202 Update GormMethodInvokingFactoryBean
    to set datastore on the service by overriding invokeWithTargetException.

--- grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/config/GormMethodInvokingFactoryBean.groovy
+++ grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/config/GormMethodInvokingFactoryBean.groovy
@@ -1,6 +1,8 @@
 package org.grails.datastore.mapping.config
 
 import groovy.transform.Internal
+import org.grails.datastore.mapping.core.Datastore
+import org.grails.datastore.mapping.services.Service
 import org.springframework.beans.factory.config.MethodInvokingFactoryBean
 
 /**
@@ -12,5 +14,14 @@ class GormMethodInvokingFactoryBean extends MethodInvokingFactoryBean {
     @Override
     Class<?> getObjectType() {
         arguments[0] as Class<?>
+    }
+
+    @Override
+    protected Object invokeWithTargetException() throws Exception {
+        Object object = super.invokeWithTargetException()
+        if (object) {
+            ((Service) object).setDatastore((Datastore) targetObject)
+        }
+        object
     }
 }
