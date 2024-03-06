60c39f3e04ca545e0953fdad70cbc4642d75675c
jhron <jhron@users.noreply.github.com>
2024-03-06 18:01:00 +0100

    Added TenantDataSourceConfig annotation to add possibility exclude datasource to be used by multi-tenancy. (#1380)

---
+++ grails-datastore-core/src/main/groovy/org/grails/datastore/mapping/multitenancy/TenantDataSourceConfig.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.datastore.mapping.multitenancy;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>An annotation that adds possibility to configure whether a domain should be excluded from particular
+ * data source(s). For example:</p>
+ *
+ * <pre>
+ * {@literal @}TenantDataSourceConfig(dataSourcesToExclude=["adminDataSource"])
+ * class Animal implements MultiTenant<Animal> {
+ *       ...
+ * }
+ * </pre>
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface TenantDataSourceConfig {
+    String[] dataSourcesToExclude() default "";
+}
